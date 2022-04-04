b87f7ad8d213d911ebf17c36bdcfc49f28f55446
Zaid Khan <zaidk@sahaj.ai>
2022-04-04 23:53:16 +0530

    Zaid, Dharun: Refactored Server

--- src/test/kotlin/ServerUnitTest.kt
+++ src/test/kotlin/ServerUnitTest.kt
@@ -1,38 +1,38 @@
-import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
-class SeverUnitTest {
+class ServerUnitTest {
 
     @Test
     fun shouldBeAbleToParseJsonArray() {
         val values = """[
-  {
-    "fieldName": "ProductId",
-    "type": "AlphaNumeric",
-    "length": 5
-  },
-  {
-    "fieldName": "Price",
-    "type": "Number"
-  },
-  {
-    "fieldName": "Export",
-    "type": "Boolean",
-    "values": [
-      "Y",
-      "N"
-    ]
-  },
-  {
-    "fieldName": "Source",
-    "type": "Alphabets"
-  }
- ]
-"""
+              {
+                "fieldName": "ProductId",
+                "type": "AlphaNumeric",
+                "length": 5
+              },
+              {
+                "fieldName": "Price",
+                "type": "Number"
+              },
+              {
+                "fieldName": "Export",
+                "type": "Boolean",
+                "values": [
+                  "Y",
+                  "N"
+                ]
+              },
+              {
+                "fieldName": "Source",
+                "type": "Alphabets"
+              }
+             ]
+            """.trimMargin()
         val server = Server(3001)
         val jsonArray = server.getMetaData(values)
         val data = jsonArray[0]
 
-        assertEquals("ProductId", data.fieldName)
+        Assertions.assertEquals("ProductId", data.fieldName)
     }
 }
