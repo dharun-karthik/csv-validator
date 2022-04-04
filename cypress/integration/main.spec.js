3984f45b3b9c5d4944b099d5b5f37b8def57b94b
Soumya Kesarapu <soumyak@sahaj.ai>
2022-04-05 00:08:56 +0530

    Added end-to-end test for front end code

---
+++ cypress/integration/main.spec.js
@@ -0,0 +1,24 @@
+describe('cypress connect test',()=>{
+    it('Open link',()=>{
+        cy.visit("http://localhost:3000")
+    })
+})
+
+describe('Send the config file',()=>
+{
+    it('fill the values and submit the config file',()=>
+    {
+        cy.get('#field').type('Hello')
+        cy.get('#type').type('alphanumeric')
+        cy.get('#text_file_id').selectFile('/Users/soumya/csv_parse/src/main/public/data.csv',{
+        })
+        cy.get('#value-submit-button').click()
+    })
+})
+
+describe('upload csv-file',()=>{
+    it('check if csv file sucessfully uploaded',()=>{
+        cy.get('#csv_id').selectFile('/Users/soumya/csv_parse/src/main/public/data.csv')
+        cy.get('#csv-submit-button').click()
+    })
+})
