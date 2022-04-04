4db59ebfc2abd6d1c6c4bf6f265a410a493deb14
Zaid Khan <zaidk@sahaj.ai>
2022-04-04 23:42:58 +0530

    Zaid, Dharun: Duplication Feature

---
+++ src/main/kotlin/validation/DuplicationValidation.kt
@@ -0,0 +1,15 @@
+package validation
+
+import org.json.JSONArray
+
+class DuplicationValidation {
+    fun getDuplicateRowNumberInJSON(dataInJSONArray: JSONArray): List<List<Int>> {
+        val mapOfIndexOfDuplicateObjects: MutableMap<String, MutableList<Int>> = mutableMapOf()
+        dataInJSONArray.forEachIndexed { index, element ->
+            mapOfIndexOfDuplicateObjects.putIfAbsent(element.toString(), mutableListOf())
+            mapOfIndexOfDuplicateObjects[element.toString()]!!.add(index+1)
+        }
+        val mapOfIndexOfDuplicateObjectsWithOnlyDuplicateValues = mapOfIndexOfDuplicateObjects.filterValues { it.size > 1 }
+        return mapOfIndexOfDuplicateObjectsWithOnlyDuplicateValues.values.toList()
+    }
+}
