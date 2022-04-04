e557d7349f8bcfba5a14f5f0ede4415e4b14c74c
Gaurav Mishra <gauravm@sahaj.ai>
2022-04-05 01:12:49 +0530

    Tested dropdown options

--- cypress/integration/main.spec.js
+++ cypress/integration/main.spec.js
@@ -9,23 +9,26 @@ describe('Send the config file',()=>
     it('fill the values and submit the config file',()=>
     {
         cy.get('#field').type('Hello')
-        cy.get('#type').type('alphanumeric')
-        cy.get('#text_file_id').selectFile('/Users/soumya/csv_parse/src/main/public/data.csv',{
+        cy.get('select').select(1)
+        cy.get('#text_file_id').selectFile('/Users/gauravm/Desktop/csv_parse/src/main/public/data.csv',{
         })
+        cy.get('#max-len').type(19)
+        cy.get('#min-len').type(7)
         cy.get('#value-submit-button').click()
     })
 })
 
+describe('check dropdown option values',()=>
+{
+    it('check the value of drop down button',()=>
+   {
+        cy.get('select').select(1).should('have.value','Number')
+    })
+})
 describe('upload csv-file',()=>{
     it('check if csv file sucessfully uploaded',()=>{
-        cy.get('#csv_id').selectFile('/Users/soumya/csv_parse/src/main/public/data.csv')
+        cy.get('#csv_id').selectFile('/Users/gauravm/Desktop/csv_parse/src/main/public/data.csv')
         cy.get('#csv-submit-button').click()
     })
 })
 
-describe('upload csv-file',()=>{
-    it('check if csv file sucessfully uploaded',()=>{
-        cy.readFile('/Users/soumya/csv_parse/src/main/public/data.csv').its('ProductID').should('12')
-
-    })
-})
