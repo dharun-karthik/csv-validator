ef9e9017d6fc02ee1814046702f23bb856294593
Mr_901 <abhishekk@sahaj.ai>
2022-04-04 22:50:45 +0530

    added maxLength in lengthValidation

---
+++ src/main/kotlin/validation/LengthValidation.kt
@@ -0,0 +1,4 @@
+package validation
+
+class LengthValidation {
+}
---
+++ src/test/kotlin/validation/LengthValidationTest.kt
@@ -0,0 +1,12 @@
+package validation
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class LengthValidationTest {
+
+    @Test
+    fun maxLength() {
+    }
+}
