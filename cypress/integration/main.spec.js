7d27a3410f248d4474d60c87e11e69d3e6b186ed
Gaurav Mishra <gauravm@sahaj.ai>
2022-04-05 11:24:19 +0530

    Added public/assets and changed path from absolute to relative path in main.spec.js

--- cypress/integration/main.spec.js
+++ cypress/integration/main.spec.js
@@ -1,6 +1,6 @@
 describe('cypress connect test',()=>{
     it('Open link',()=>{
-        cy.visit("http://localhost:3002")
+        cy.visit("http://localhost:3000")
     })
 })
 
@@ -10,7 +10,7 @@ describe('Send the config file',()=>
     {
         cy.get('#field').type('Hello')
         cy.get('select').select(1)
-        cy.get('#text_file_id').selectFile('/Users/soumya/csv_parse/src/main/public/data.csv',{
+        cy.get('#text_file_id').selectFile('src/main/public/assets/sample3.txt',{
         })
         cy.get('#max-len').type(19)
         cy.get('#min-len').type(7)
@@ -34,7 +34,7 @@ describe('check dropdown option values',()=>
 
 describe('upload csv-file',()=>{
     it('check if csv file successfully uploaded',()=>{
-        cy.get('#csv_id').selectFile('/Users/soumya/csv_parse/src/main/public/data.csv')
+        cy.get('#csv_id').selectFile('src/main/public/assets/data.csv')
         cy.get('#csv-submit-button').click()
     })
 })
