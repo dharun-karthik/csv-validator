10f7fe4485a20399f0f71c5385def24e0c576b53
Mr_901 <abhishekk@sahaj.ai>
2022-04-05 10:59:05 +0530

    added ResponseHead Tests

---
+++ src/test/kotlin/routeHandler/ResponseHeadTest.kt
@@ -0,0 +1,37 @@
+package routeHandler
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class ResponseHeadTest {
+
+    @Test
+    fun shouldReturnHttpResponseHeadFor200StatusCode() {
+        val responseHead = ResponseHead()
+        val expected = "HTTP/1.1 200 Found\n"
+
+        val actual = responseHead.getHttpHead(200)
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun shouldReturnHttpResponseHeadFor400StatusCode() {
+        val responseHead = ResponseHead()
+        val expected = "HTTP/1.1 400 Bad Request\n"
+
+        val actual = responseHead.getHttpHead(400)
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun shouldReturnHttpResponseHeadFor401StatusCode() {
+        val responseHead = ResponseHead()
+        val expected = "HTTP/1.1 401 Unauthorized\n"
+
+        val actual = responseHead.getHttpHead(401)
+
+        assertEquals(expected, actual)
+    }
+}
