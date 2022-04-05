26e4ba02dfb020a073627bb764cdac67654c5585
Mr_901 <abhishekk@sahaj.ai>
2022-04-05 10:43:37 +0530

    added responseHead in constructor of GitRouteHandler

--- src/main/kotlin/routeHandler/Get.kt
+++ src/main/kotlin/routeHandler/Get.kt
@@ -2,13 +2,8 @@ package routeHandler
 
 import java.io.File
 
-class Get {
+class Get(private val responseHead: ResponseHead = ResponseHead()) {
 
-    private val statusMap = mapOf(
-        200 to "Found",
-        400 to "Bad Request",
-        401 to "Unauthorized"
-    )
 
     fun handleGetRequest(request: String): String {
         val path = getPath(request)
@@ -22,7 +17,7 @@ class Get {
         val responseBody = readFileContent(path)
         val contentLength = responseBody.length
         val endOfHeader = "\r\n\r\n"
-        return getHttpHead(200) + """Content-Type: text/html; charset=utf-8
+        return responseHead.getHttpHead(200) + """Content-Type: text/html; charset=utf-8
             |Content-Length: $contentLength""".trimMargin() + endOfHeader + responseBody
     }
 
@@ -39,9 +34,5 @@ class Get {
         return request.split("\r\n")[0].split(" ")[1].substringBefore("?")
     }
 
-    private fun getHttpHead(statusCode: Int): String {
-        val content = statusMap[statusCode]
-        return "HTTP/1.1 $statusCode $content\n"
-    }
 
 }
