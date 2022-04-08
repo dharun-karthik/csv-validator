7e98f9a63a59a74ab912f26a14b0faccadd8d21c
Shobhit Aryan <shobhitl@sahaj.ai>
2022-04-08 21:38:47 +0530

    Refactored MetaDataReaderWriterTest

--- src/test/kotlin/metaData/MetaDataReaderWriterTest.kt
+++ src/test/kotlin/metaData/MetaDataReaderWriterTest.kt
@@ -70,6 +70,7 @@ class MetaDataReaderWriterTest {
     ]
   }
 ]"""
+
         val actual = metaDataReaderWriter.readRawContent()
 
         assertEquals(expected, actual)
@@ -79,8 +80,8 @@ class MetaDataReaderWriterTest {
     fun shouldBeAbleToGiveMetaDataInJson() {
         val metaDataReaderWriter = MetaDataReaderWriter("src/test/kotlin/metaDataTestFiles/csv-meta-data-test.json")
         val fields = metaDataReaderWriter.readFields()
-
         val expected = "500020"
+
         val actual = fields[7].values?.get(0)
 
         assertEquals(expected, actual)
@@ -89,6 +90,7 @@ class MetaDataReaderWriterTest {
     @Test
     fun shouldBeAbleToGiveMetaDataInJsonWhenThereIsNoContent() {
         val metaDataReaderWriter = MetaDataReaderWriter("src/test/kotlin/metaDataTestFiles/empty-json-test.json")
+
         val expected = metaDataReaderWriter.readFields()
 
         assertNotNull(expected)
@@ -100,9 +102,9 @@ class MetaDataReaderWriterTest {
         val jsonData: Array<JsonMetaDataTemplate> =
             arrayOf(JsonMetaDataTemplate("test field", "Alphabet", 2, 1, 3, listOf("22")))
         metaDataReaderWriter.writeJsonContent(jsonData)
-
         val expected =
             """[{"fieldName":"test field","type":"Alphabet","length":2,"minLength":1,"maxLength":3,"values":["22"]}]"""
+
         val actual = metaDataReaderWriter.readRawContent()
 
         assertEquals(expected, actual)
@@ -114,13 +116,13 @@ class MetaDataReaderWriterTest {
         val oldField = """{"fieldName":"test field","type":"Alphabet","length":2,"minLength":1,"maxLength":3,"values":["22"]}"""
         metaDataReaderWriter.appendField(oldField)
         val field = """{"fieldName": "ProductDescription","type": "AlphaNumeric","minLength": 7,"maxLength": 20}"""
+        metaDataReaderWriter.appendField(field)
         val expected =
             """[{"fieldName":"test field","type":"Alphabet","length":2,"minLength":1,"maxLength":3,"values":["22"]},{"fieldName":"ProductDescription","type":"AlphaNumeric","minLength":7,"maxLength":20}]"""
 
-        metaDataReaderWriter.appendField(field)
         val actual = metaDataReaderWriter.readRawContent()
-        metaDataReaderWriter.clearFields()
 
         assertEquals(expected, actual)
+        metaDataReaderWriter.clearFields()
     }
 }
