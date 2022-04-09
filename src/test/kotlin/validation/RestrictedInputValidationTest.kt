e594d144ee151258feaa596ab36439724432ae49
Shobhit Aryan <shobhitl@sahaj.ai>
2022-04-09 18:55:42 +0530

    Restructured the test according to AAA

--- src/test/kotlin/validation/RestrictedInputValidationTest.kt
+++ src/test/kotlin/validation/RestrictedInputValidationTest.kt
@@ -5,11 +5,14 @@ import org.junit.jupiter.api.Test
 
 internal class RestrictedInputValidationTest {
     private val restrictedInputList = listOf("Y", "N", "y", "n");
+
     @Test
     fun shouldReturnTrueIfInputTextIsPresentInRestrictedInputList() {
         val restrictedInputValidation = RestrictedInputValidation()
         val inputText = "Y"
+
         val actual = restrictedInputValidation.validate(inputText, restrictedInputList)
+
         assertTrue(actual)
     }
 }
