0b81e255de829319bbea6a626f51cda9c781cc4e
Shobhit Aryan <shobhitl@sahaj.ai>
2022-04-09 18:22:57 +0530

    Added tests for Numbers

---
+++ src/test/kotlin/valueValidator/NumbersTest.kt
@@ -0,0 +1,29 @@
+package valueValidator
+
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+import validation.TypeValidation
+
+internal class NumbersTest {
+    private val numbers = Numbers()
+    private val typeValidation = TypeValidation()
+
+    @Test
+    fun shouldReturnTrueIfInputTextIsNumeric() {
+        val inputText = "1"
+
+        val actual = numbers.validateValueType(inputText, typeValidation)
+
+        assertTrue(actual)
+    }
+
+    @Test
+    fun shouldReturnFalseIfInputTextIsNotNumeric() {
+        val inputText = "123aA:?"
+
+        val actual = numbers.validateValueType(inputText, typeValidation)
+
+        assertFalse(actual)
+    }
+}
