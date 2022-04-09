05a57e7c52751def116e04b3227bb0ea82e63736
pratheepmm <pratheepm@sahaj.ai>
2022-04-09 13:21:25 +0530

    added more tests

--- src/test/kotlin/validation/DependencyValidationTest.kt
+++ src/test/kotlin/validation/DependencyValidationTest.kt
@@ -1,5 +1,6 @@
 package validation
 
+import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
@@ -7,8 +8,47 @@ class DependencyValidationTest {
     @Test
     fun shouldGetTrueWhenDependencyIsValid(){
         val dependencyValidation = DependencyValidation()
-        val currentField = FieldValue("Country Name","null")
+        val currentField = FieldValue("Country Name",null)
         val dependentField = FieldValue("Export","N")
+        val expectedDependentFieldValue = "N"
+        val expectedCurrentFieldValue = null
+
+        val actual = dependencyValidation.validate(currentField,dependentField,expectedDependentFieldValue,expectedCurrentFieldValue)
+
+        assertTrue(actual)
+    }
+
+    @Test
+    fun shouldGetFalseWhenDependencyIsInValid(){
+        val dependencyValidation = DependencyValidation()
+        val currentField = FieldValue("Country Name","AUS")
+        val dependentField = FieldValue("Export","N")
+        val expectedDependentFieldValue = "N"
+        val expectedCurrentFieldValue = null
+
+        val actual = dependencyValidation.validate(currentField,dependentField,expectedDependentFieldValue,expectedCurrentFieldValue)
+
+        assertFalse(actual)
+    }
+
+    @Test
+    fun shouldGetTrueWhenDependencyFieldValueIsNotMet(){
+        val dependencyValidation = DependencyValidation()
+        val currentField = FieldValue("Country Name","AUS")
+        val dependentField = FieldValue("Export","Y")
+        val expectedDependentFieldValue = "N"
+        val expectedCurrentFieldValue = null
+
+        val actual = dependencyValidation.validate(currentField,dependentField,expectedDependentFieldValue,expectedCurrentFieldValue)
+
+        assertTrue(actual)
+    }
+
+    @Test
+    fun shouldPassDespiteOfCaseSensitivity(){
+        val dependencyValidation = DependencyValidation()
+        val currentField = FieldValue("Country Name",null)
+        val dependentField = FieldValue("Export","n")
         val expectedDependentFieldValue = "N"
         val expectedCurrentFieldValue = null
 
