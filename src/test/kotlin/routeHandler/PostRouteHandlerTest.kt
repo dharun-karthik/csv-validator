e8b3e311bdba1485fa53f779ff53c31bc6c8067c
pratheepmm <pratheepm@sahaj.ai>
2022-04-09 16:39:40 +0530

    added test case for dependency functionality

--- src/test/kotlin/routeHandler/PostRouteHandlerTest.kt
+++ src/test/kotlin/routeHandler/PostRouteHandlerTest.kt
@@ -126,4 +126,38 @@ class PostRouteHandlerTest {
 
         assertEquals(expected.toString(), result.toString())
     }
+
+    @Test
+    fun shouldGiveDependencyErrorLinesAsResult(){
+        val metaDataReaderWriter = MetaDataReaderWriter("src/test/kotlin/metaDataTestFiles/csv-meta-data-test.json")
+        val postRouteHandler = PostRouteHandler(metaDataReaderWriter)
+        val csvData = """[
+    {
+        "Product Id": "1564",
+        "Product Description": "Table",
+        "Price": "4500.59",
+        "Export": "N",
+        "Country Name","AUS",
+        "Source City": "Nagpur",
+        "Source Pincode": "440001"
+    },
+    {
+        "Product Id": "1234",
+        "Product Description": "Chairs",
+        "Price": "1000",
+        "Export": "Y",
+        "Country Name": "AUS",
+        "Source City": "Mumbai",
+        "Country Code": "61",
+        "Source Pincode": "400001"
+    }
+]"""
+        val jsonCsvData = JSONArray(csvData)
+        val expected = JSONArray()
+        expected.put(JSONObject().put("1","Dependency Error in Country Name"))
+        val result = postRouteHandler.dependencyValidation(jsonCsvData)
+
+        assertEquals(expected.toString(), result.toString())
+    }
+
 }
