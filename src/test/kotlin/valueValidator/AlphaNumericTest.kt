aa4a96c5fbe6560c5e046f5754aa6da39f54f96f
Shobhit Aryan <shobhitl@sahaj.ai>
2022-04-09 18:16:21 +0530

    Added Tests for AlphaNumeric

--- src/test/kotlin/valueValidator/AlphaNumericTest.kt
+++ src/test/kotlin/valueValidator/AlphaNumericTest.kt
@@ -1,5 +1,46 @@
 package valueValidator
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import validation.TypeValidation
 
-internal class AlphaNumericTest
+internal class AlphaNumericTest{
+    private val alphaNumeric = AlphaNumeric()
+    private val typeValidation = TypeValidation()
+
+    @Test
+    fun shouldReturnTrueIfInputTextIsAlphaNumeric() {
+        val inputText = "abcABC12"
+
+        val actual = alphaNumeric.validateValueType(inputText, typeValidation)
+
+        assertTrue(actual)
+    }
+
+    @Test
+    fun shouldReturnFalseIfInputTextIsNotAlphaNumeric() {
+        val inputText = "abcABC1:"
+
+        val actual = alphaNumeric.validateValueType(inputText, typeValidation)
+
+        assertFalse(actual)
+    }
+
+    @Test
+    fun shouldReturnTrueIfInputTextHavingAlphabetsOnlyIsAlphaNumeric() {
+        val inputText = "abcABC"
+
+        val actual = alphaNumeric.validateValueType(inputText, typeValidation)
+
+        assertTrue(actual)
+    }
+
+    @Test
+    fun shouldReturnTrueIfInputTextHavingDigitsOnlyIsAlphaNumeric() {
+        val inputText = "1234"
+
+        val actual = alphaNumeric.validateValueType(inputText, typeValidation)
+
+        assertTrue(actual)
+    }
+}
