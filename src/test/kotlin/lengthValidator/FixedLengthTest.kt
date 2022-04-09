d1d137c3a9b3d7e6708831764f9571561dda15b1
Shobhit Aryan <shobhitl@sahaj.ai>
2022-04-09 15:10:04 +0530

    Added Tests for Fixed Length

--- src/test/kotlin/lengthValidator/FixedLengthTest.kt
+++ src/test/kotlin/lengthValidator/FixedLengthTest.kt
@@ -1,5 +1,42 @@
 package lengthValidator
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import validation.LengthValidation
 
-internal class FixedLengthTest
+internal class FixedLengthTest{
+    @Test
+    fun shouldReturnTrueIfInputTextLengthIsSameAsSpecified(){
+        val fixedLength = FixedLength();
+        val inputText = "IND"
+        val length = 3
+
+        val actual = fixedLength.validateLengthType(inputText, length, lengthValidation = LengthValidation())
+
+        assertTrue(actual)
+    }
+
+    @Test
+    fun shouldReturnFalseIfInputTextLengthIsNotSameAsSpecified(){
+        val fixedLength = FixedLength();
+        val inputText = "IND"
+        val length = 2
+
+        val actual = fixedLength.validateLengthType(inputText, length, lengthValidation = LengthValidation())
+
+        assertFalse(actual)
+    }
+
+    @Test
+    fun shouldReturnTrueIfLengthIsNull(){
+        val fixedLength = FixedLength();
+        val inputText = "IND"
+        val length = null
+
+        val actual = fixedLength.validateLengthType(inputText, length, lengthValidation = LengthValidation())
+
+        assertTrue(actual)
+    }
+
+
+}
