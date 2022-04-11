9514cf745050a495624038350e0c8a29c0e88f83
pratheepmm <pratheepm@sahaj.ai>
2022-04-11 16:37:51 +0530

    removed \"

--- src/test/kotlin/validation/ConfigValidationTest.kt
+++ src/test/kotlin/validation/ConfigValidationTest.kt
@@ -9,9 +9,9 @@ class ConfigValidationTest {
     @Test
     fun shouldReturnEmptyJsonArrayIfAllConfigColumnsArePresentInCsvData() {
         val configJSON =
-            "[{\"fieldName\":\"ProductID\",\"type\":\"Number\",\"length\":\"\",\"minLength\":\"\",\"maxLength\":\"\"},{\"fieldName\":\"ProductName\",\"type\":\"Alphabet\",\"length\":\"\",\"minLength\":\"\",\"maxLength\":\"\"}]"
+            """[{"fieldName":"ProductID","type":"Number","length":"","minLength":"","maxLength":""},{"fieldName":"ProductName","type":"Alphabet","length":"","minLength":"","maxLength":""}]"""
         val jsonData = JSONArray(
-            "[{\"ProductID\":\"1564\",\"ProductName\":\"Table\",\"Price\":\"4500.59\",\"Export\":\"N\",\"CountryName\":\"Nagpur\",\"SourceCity\":\"440001\"},{\"ProductID\":\"1234\",\"ProductName\":\"Chairs\",\"Price\":\"1000\",\"Export\":\"Y\",\"CountryName\":\"AUS\",\"SourceCity\":\"Mumbai\",\"CountryCode\":\"61\",\"SourcePincode\\r\":\"400001\"},{\"ProductID\":\"\"}]"
+            """[{"ProductID":"1564","ProductName":"Table","Price":"4500.59","Export":"N","CountryName":"Nagpur","SourceCity":"440001"},{"ProductID":"1234","ProductName":"Chairs","Price":"1000","Export":"Y","CountryName":"AUS","SourceCity":"Mumbai","CountryCode":"61","SourcePincode\\r":"400001"},{"ProductID":""}]"""
         )
         val columnValidator = ColumnValidation()
         val expected = "[]"
@@ -25,12 +25,12 @@ class ConfigValidationTest {
     @Test
     fun shouldReturnOneColumnNameIfOneConfigColumnIsNotInCsvData() {
         val configJSON =
-            "[{\"fieldName\":\"ProductIDNumber\",\"type\":\"Number\",\"length\":\"\",\"minLength\":\"\",\"maxLength\":\"\"},{\"fieldName\":\"ProductName\",\"type\":\"Alphabet\",\"length\":\"\",\"minLength\":\"\",\"maxLength\":\"\"}]"
+            """[{"fieldName":"ProductIDNumber","type":"Number","length":"","minLength":"","maxLength":""},{"fieldName":"ProductName","type":"Alphabet","length":"","minLength":"","maxLength":""}]"""
         val jsonData = JSONArray(
-            "[{\"ProductID\":\"1564\",\"ProductName\":\"Table\",\"Price\":\"4500.59\",\"Export\":\"N\",\"CountryName\":\"Nagpur\",\"SourceCity\":\"440001\"},{\"ProductID\":\"1234\",\"ProductName\":\"Chairs\",\"Price\":\"1000\",\"Export\":\"Y\",\"CountryName\":\"AUS\",\"SourceCity\":\"Mumbai\",\"CountryCode\":\"61\",\"SourcePincode\\r\":\"400001\"},{\"ProductID\":\"\"}]"
+            """[{"ProductID":"1564","ProductName":"Table","Price":"4500.59","Export":"N","CountryName":"Nagpur","SourceCity":"440001"},{"ProductID":"1234","ProductName":"Chairs","Price":"1000","Export":"Y","CountryName":"AUS","SourceCity":"Mumbai","CountryCode":"61","SourcePincode\\r":"400001"},{"ProductID":""}]"""
         )
         val columnValidator = ColumnValidation()
-        val expected = "[{\"Column Name Error\":\"ProductIDNumber\"}]"
+        val expected = """[{"Column Name Error":"ProductIDNumber"}]"""
 
         val actual = columnValidator.getInvalidFieldNames(configJSON, jsonData).toString()
 
@@ -41,12 +41,12 @@ class ConfigValidationTest {
     @Test
     fun shouldReturnTwoColumnNameIfTwoConfigColumnIsNotInCsvData() {
         val configJSON =
-            "[{\"fieldName\":\"ProductIDNumber\",\"type\":\"Number\",\"length\":\"\",\"minLength\":\"\",\"maxLength\":\"\"},{\"fieldName\":\"ProductNamed\",\"type\":\"Alphabet\",\"length\":\"\",\"minLength\":\"\",\"maxLength\":\"\"},{\"fieldName\":\"CountryName\",\"type\":\"Alphabet\",\"length\":\"\",\"minLength\":\"\",\"maxLength\":\"\"}]"
+            """[{"fieldName":"ProductIDNumber","type":"Number","length":"","minLength":"","maxLength":""},{"fieldName":"ProductNamed","type":"Alphabet","length":"","minLength":"","maxLength":""},{"fieldName":"CountryName","type":"Alphabet","length":"","minLength":"","maxLength":""}]"""
         val jsonData =JSONArray(
-            "[{\"ProductID\":\"1564\",\"ProductName\":\"Table\",\"Price\":\"4500.59\",\"Export\":\"N\",\"CountryName\":\"Nagpur\",\"SourceCity\":\"440001\"},{\"ProductID\":\"1234\",\"ProductName\":\"Chairs\",\"Price\":\"1000\",\"Export\":\"Y\",\"CountryName\":\"AUS\",\"SourceCity\":\"Mumbai\",\"CountryCode\":\"61\",\"SourcePincode\\r\":\"400001\"},{\"ProductID\":\"\"}]"
+            """[{"ProductID":"1564","ProductName":"Table","Price":"4500.59","Export":"N","CountryName":"Nagpur","SourceCity":"440001"},{"ProductID":"1234","ProductName":"Chairs","Price":"1000","Export":"Y","CountryName":"AUS","SourceCity":"Mumbai","CountryCode":"61","SourcePincode\\r":"400001"},{"ProductID":""}]"""
         )
         val columnValidator = ColumnValidation()
-        val expected = "[{\"Column Name Error\":\"ProductIDNumber\"},{\"Column Name Error\":\"ProductNamed\"}]"
+        val expected = """[{"Column Name Error":"ProductIDNumber"},{"Column Name Error":"ProductNamed"}]"""
 
         val actual = columnValidator.getInvalidFieldNames(configJSON, jsonData).toString()
 
