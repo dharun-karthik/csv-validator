37c91af0431129bb1cc517b25d4b5e874e2d8d9a
DharunKarthik <dharunk@sahaj.ai>
2022-04-12 17:48:04 +0530

    Wrote test for one func in RequestHandle

---
+++ src/test/kotlin/request/RequestHandleTest.kt
@@ -0,0 +1,34 @@
+package request
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+internal class RequestHandleTest{
+
+    internal class GetBodyTest{
+
+        @Test
+        fun shouldReturnPathForGetRequest(){
+            val requestHandle = RequestHandle()
+            val request = "GET /favicon.ico HTTP/1.1"
+            val expected = "/favicon.ico"
+
+            val actual = requestHandle.getPath(request)
+
+            assertEquals(expected, actual)
+        }
+
+        @Test
+        fun shouldReturnPathForPostRequest(){
+            val requestHandle = RequestHandle()
+            val request = "POST /csv-meta-data HTTP/1.1"
+            val expected = "/csv-meta-data"
+
+            val actual = requestHandle.getPath(request)
+
+            assertEquals(expected, actual)
+        }
+    }
+
+    // TODO: Write test for other two functions
+}
