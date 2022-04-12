c3fa6782da29dc92cc259208a69d4a594f79381e
DharunKarthik <dharunk@sahaj.ai>
2022-04-12 14:48:43 +0530

    Refractored server Class and added stop server functionality

---
+++ src/main/kotlin/ClientHandler.kt
@@ -0,0 +1,40 @@
+import routeHandler.RequestHandler
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.net.Socket
+
+class ClientHandler {
+
+    fun handleClient(clientSocket: Socket, requestHandler: RequestHandler) {
+        val outputStream = BufferedWriter(OutputStreamWriter(clientSocket.getOutputStream()))
+        val inputStream = BufferedReader(InputStreamReader(clientSocket.getInputStream()))
+
+        val request = readRequest(inputStream)
+        println("request $request")
+        val responseData = requestHandler.handleRequest(request, inputStream)
+
+        getResponseData(outputStream, responseData)
+
+        clientSocket.close()
+    }
+
+    private fun getResponseData(outputStream: BufferedWriter, responseData: String) {
+        outputStream.write(responseData)
+        outputStream.flush()
+    }
+
+    private fun readRequest(inputStream: BufferedReader): String {
+        var request = ""
+        var flag = true
+        while (flag) {
+            val line = inputStream.readLine()
+            request += line + "\r\n"
+            if (line == null || line.isEmpty()) {
+                flag = false
+            }
+        }
+        return request
+    }
+}
