38b7a13dec1262a2adee011fba9b878969ea622a
Shobhit Aryan <shobhitl@sahaj.ai>
2022-04-15 11:44:38 +0530

    Added functionality to validate if value is YYYY-MM-DD date format

--- src/main/kotlin/validation/TypeValidation.kt
+++ src/main/kotlin/validation/TypeValidation.kt
@@ -17,4 +17,8 @@ class TypeValidation {
     fun isDecimal(value: String): Boolean {
         return value.matches("""^[+-]?(\d+\.?\d*|\.\d+)${'$'}""".toRegex())
     }
+
+    fun `isDateInYYYYMMDDFormat`(value: String): Boolean {
+        return value.matches("""^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])${'$'}""".toRegex())
+    }
 }
