2e463f848cce362a37e2ff60da9fc29134e4a663
DharunKarthik <dharunk@sahaj.ai>
2022-04-17 18:29:13 +0530

    Implemented dateTime Validation

--- src/main/kotlin/validation/valueValidator/DateTimeValidator.kt
+++ src/main/kotlin/validation/valueValidator/DateTimeValidator.kt
@@ -1,4 +1,18 @@
 package validation.valueValidator
 
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
 class DateTimeValidator {
+    fun validate(pattern: String, dateTimeValue: String): Boolean {
+        try {
+            val lowerCasedDateTime = dateTimeValue.lowercase()
+            val format = DateTimeFormatter.ofPattern(pattern)
+            LocalDateTime.parse(lowerCasedDateTime, format)
+            return true
+        } catch (e : Exception){
+            println(e)
+            return false
+        }
+    }
 }
---
+++ src/test/kotlin/validation/valueValidator/DateTimeValidatorTest.kt
@@ -0,0 +1,61 @@
+package validation.valueValidator
+
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+internal class DateTimeValidatorTest {
+
+
+    @ParameterizedTest
+    @MethodSource("validDateTimeArguments")
+    fun shouldReturnTrueWhenValidDateTimeAndPatternIsGiven(pattern: String, dateTimeValue: String) {
+        val dateTimeValidator = DateTimeValidator()
+
+        val actual = dateTimeValidator.validate(pattern, dateTimeValue)
+
+        assertTrue(actual)
+    }
+
+    @ParameterizedTest
+    @MethodSource("inValidDateTimeArguments")
+    fun shouldReturnFalseWhenInValidDateTimeAndPatternIsGiven(pattern: String, dateTimeValue: String) {
+        val dateTimeValidator = DateTimeValidator()
+
+        val actual = dateTimeValidator.validate(pattern, dateTimeValue)
+
+        assertFalse(actual)
+    }
+
+    private fun validDateTimeArguments(): List<Arguments> {
+        return listOf(
+            Arguments.of("HH:mm:ss dd/MM/yyyy", "00:12:18 20/07/2000"),
+            Arguments.of("HH:mm:ss?dd/yyyy/MM", "23:59:58?20/2000/07"),
+            Arguments.of("HH:ss:mm:dd:MM:yyyy", "23:58:59:20:07:2000"),
+            Arguments.of("ss:HH:mm/yyyy/MM/dd", "56:23:35/2000/07/20"),
+            Arguments.of("HH:ss:mm-dd-yyyy-MM", "23:58:59-20-2000-07"),
+            Arguments.of("HH:ss:mm:SSS dd MM yyyy", "23:58:59:978 20 07 2000"),
+            Arguments.of("hh:ss:mm a,yyyy-MM-dd", "12:00:58 AM,2000-07-20"),
+            Arguments.of("hh:ss:mma+dd/MM/yyyy", "12:00:58AM+20/07/2000"),
+            Arguments.of("ahh:ss:mm () dd/MM/yyyy", "aM12:00:58 () 20/07/2000"),
+            Arguments.of("dd/MM/yyyy hh:ass:mm", "20/07/2000 12:AM00:58"),
+            Arguments.of("dd/MM/yyyy == hh:ass:mm:SSS", "20/07/2000 == 12:am00:58:917"),
+            Arguments.of("dd MM yyyy hh:ass:mm", "20 07 2001 12:aM00:58"),
+        )
+    }
+
+    private fun inValidDateTimeArguments(): List<Arguments> {
+        return listOf(
+            Arguments.of("HH:mm:ss dd/MM/yyyy", "25:34:58 12/02/2000"),
+            Arguments.of("HH:mm:ss dd/MM/yyyy", "00:12:18/28/07/2000"),
+            Arguments.of("hh:ss:mm a-dd-MM-yyyy", "12:00:58 am-33/07/2000"),
+            Arguments.of("dd-MM/yyyy,HH:ss:mm", "12/16/2000,0:58:36"),
+            Arguments.of("HH:mm:ss", "13:00:00"),
+            Arguments.of("dd-MM-yyyy", "12/08/2000")
+        )
+    }
+}
