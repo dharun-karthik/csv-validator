6cab13406810c81076af4f423e0969f9808e0475
pratheepmm <pratheepm@sahaj.ai>
2022-04-17 17:57:40 +0530

    removed unused package

--- src/test/kotlin/validation/valueValidator/DateValidatorTest.kt
+++ src/test/kotlin/validation/valueValidator/DateValidatorTest.kt
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
-import validation.valueValidator.DateValidator
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class DateValidatorTest {
