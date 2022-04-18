5b120c9c366be4caec7c4145682d06623ae02654
DharunKarthik <dharunk@sahaj.ai>
2022-04-18 07:26:55 +0530

    Removed unused function of checking Invalid column Name

--- src/main/kotlin/validation/implementation/ColumnValidation.kt
+++ src/main/kotlin/validation/implementation/ColumnValidation.kt
@@ -6,36 +6,6 @@ import org.json.JSONObject
 
 class ColumnValidation {
 
-    fun getInvalidFieldNames(metaDataJson: String, jsonDataArray: JSONArray): JSONArray {
-        val metaDataJsonArray = JSONArray(metaDataJson)
-        val errorJsonArray = JSONArray()
-        val fieldsInJsonData = getAllFieldNamesOfJsonData(jsonDataArray)
-        val noOfConfig = metaDataJsonArray.length()
-        for (index in 0 until noOfConfig) {
-            val configJsonObject = metaDataJsonArray.getJSONObject(index)
-            val configFieldName = configJsonObject.get("fieldName").toString()
-            if (!fieldsInJsonData.contains(configFieldName.lowercase())) {
-                val obj = JSONObject()
-                obj.put("Column Name Error", configFieldName)
-                errorJsonArray.put(obj)
-            }
-        }
-        return errorJsonArray
-
-    }
-
-    private fun getAllFieldNamesOfJsonData(jsonDataArray: JSONArray): List<String> {
-        val fieldNames = mutableListOf<String>()
-        if (jsonDataArray.isEmpty) {
-            return fieldNames
-        }
-        val jsonObjectKeyIterator = jsonDataArray.getJSONObject(0).keys()
-        while (jsonObjectKeyIterator.hasNext()) {
-            fieldNames.add(jsonObjectKeyIterator.next().lowercase())
-        }
-        return fieldNames
-    }
-
     fun getColumnsNotInConfig(metaDataJson: String, jsonDataArray: JSONArray): JSONArray {
         val metaDataJsonArray = JSONArray(metaDataJson)
         val unavailableColumnJsonArray = JSONArray()
@@ -50,6 +20,18 @@ class ColumnValidation {
             unavailableColumnJsonArray.put(obj)
         }
         return unavailableColumnJsonArray
+    }
+
+    private fun getAllFieldNamesOfJsonData(jsonDataArray: JSONArray): List<String> {
+        val fieldNames = mutableListOf<String>()
+        if (jsonDataArray.isEmpty) {
+            return fieldNames
+        }
+        val jsonObjectKeyIterator = jsonDataArray.getJSONObject(0).keys()
+        while (jsonObjectKeyIterator.hasNext()) {
+            fieldNames.add(jsonObjectKeyIterator.next().lowercase())
+        }
+        return fieldNames
     }
 
     private fun getAllFieldNamesOfMetaData(metaDataJsonArray: JSONArray): List<String> {
