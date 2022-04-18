561bc27c70056285506530b0438654a71ed4f14c
pratheepmm <pratheepm@sahaj.ai>
2022-04-18 12:44:32 +0530

    added gradle run script

--- src/main/kotlin/Server.kt
+++ src/main/kotlin/Server.kt
@@ -4,7 +4,7 @@ import java.net.ServerSocket
 
 
 class Server(
-    port: Int = 3000,
+    private val port: Int = 3000,
     metaDataPath: String = "src/main/public/csv-meta-data.json"
 ) {
     private val metaDataReaderWriter = MetaDataReaderWriter(metaDataPath)
@@ -12,6 +12,7 @@ class Server(
     private val serverSocket = ServerSocket(port)
 
     fun startServer() {
+        println("Sever Running in port $port")
         while (true) {
             val clientSocket = serverSocket.accept()
             ClientHandler().handleClient(clientSocket, requestHandler)
