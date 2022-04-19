3d673d04576d6b9d7e5eb9e6594ac4d2ad43fec8
pratheepmm <pratheepm@sahaj.ai>
2022-04-19 22:16:32 +0530

    implemented email validation

--- src/main/kotlin/validation/implementation/valueValidator/EmailValidator.kt
+++ src/main/kotlin/validation/implementation/valueValidator/EmailValidator.kt
@@ -1,2 +1,8 @@
-package validation.implementation.valueValidator 
+package validation.implementation.valueValidator
 
+class EmailValidator : ValueTypeValidator {
+    override fun validate(value: String, pattern: String?): Boolean {
+        return value.matches("""^(.+)@(.+)${'$'}""".toRegex())
+    }
+
+}
