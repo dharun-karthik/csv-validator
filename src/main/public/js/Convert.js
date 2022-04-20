9f6b8bac31b86eb6861cf61ad22ae336eb4ba402
Shobhit Aryan <shobhitl@sahaj.ai>
2022-04-20 12:01:17 +0530

    Shifted convertPayloadToJsonArray function from app.js to convert.js

--- src/main/public/js/Convert.js
+++ src/main/public/js/Convert.js
@@ -20,3 +20,35 @@ function csvToJson(lines) {
     }
     return result;
 }
+
+
+function arrangeDependencies(payload) {
+    for (let field in payload) {
+        for (let key in payload[field]) {
+            if (key == "dependentOn") {
+                assignDependencies(payload, field);
+            }
+        }
+    }
+    return payload;
+}
+
+function assignDependencies(payload, field) {
+    payload[field]["dependencies"] = [{
+        "dependentOn": payload[field]["dependentOn"],
+        "expectedDependentFieldValue": payload[field]["expectedDependentFieldValue"],
+        "expectedCurrentFieldValue": payload[field]["expectedCurrentFieldValue"]
+    }];
+}
+
+function convertPayloadToJsonArray(payload) {
+    console.log(`payload ${payload}`)
+    let jsonArray = []
+    let index = 0;
+    payload = arrangeDependencies(payload);
+    for (let field in payload) {
+        jsonArray[index++] = payload[field];
+    }
+    console.log(jsonArray);
+    return jsonArray;
+}
--- src/main/public/js/app.js
+++ src/main/public/js/app.js
@@ -332,36 +332,6 @@ function lowerCase(data, field = "") {
     return String(data).toLowerCase()
 }
 
-function arrangeDependencies(payload) {
-    for (let field in payload) {
-        for (let key in payload[field]) {
-            if (key == "dependentOn") {
-                assignDependencies(payload, field);
-            }
-        }
-    }
-    return payload;
-}
-
-function assignDependencies(payload, field) {
-    payload[field]["dependencies"] = [{
-        "dependentOn": payload[field]["dependentOn"],
-        "expectedDependentFieldValue": payload[field]["expectedDependentFieldValue"],
-        "expectedCurrentFieldValue": payload[field]["expectedCurrentFieldValue"]
-    }];
-}
-
-function convertPayloadToJsonArray(payload) {
-    let jsonArray = []
-    let index = 0;
-    payload = arrangeDependencies(payload);
-    for (let field in payload) {
-        jsonArray[index++] = payload[field];
-    }
-    console.log(jsonArray);
-    return jsonArray;
-}
-
 async function sendConfigData() {
     let isInputValid = validateInputFields()
     if (!isInputValid) {
