51958390e12f0fa5c931ec0bde482332d0c0af4b
pratheepmm <pratheepm@sahaj.ai>
2022-04-20 09:16:56 +0530

    added invalid email test cases

--- src/test/kotlin/validation/implementation/valueValidator/EmailValidatorTest.kt
+++ src/test/kotlin/validation/implementation/valueValidator/EmailValidatorTest.kt
@@ -1,5 +1,6 @@
 package validation.implementation.valueValidator
 
+import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.params.ParameterizedTest
@@ -7,7 +8,7 @@ import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-class EmailValidatorTest {
+    class EmailValidatorTest {
 
     @ParameterizedTest
     @MethodSource("validEmailArguments")
@@ -17,6 +18,29 @@ class EmailValidatorTest {
         val actual = emailValidator.validate(emailValue)
 
         assertTrue(actual)
+    }
+
+    @ParameterizedTest
+    @MethodSource("inValidEmailArguments")
+    fun shouldReturnFalseWhenEmailIsInValid(emailValue: String) {
+        val emailValidator = EmailValidator()
+
+        val actual = emailValidator.validate(emailValue)
+
+        assertFalse(actual)
+    }
+
+    private fun inValidEmailArguments(): List<Arguments>{
+        return listOf(
+            Arguments.of("Abc.example.com"),
+            Arguments.of("A@b@c@example.com"),
+            Arguments.of("a\"b(c)d,e:f;g<h>i[j\\k]l@example.com"),
+            Arguments.of("just\"not\"right@example.com"),
+            Arguments.of("this is\"not\\allowed@example.com"),
+            Arguments.of("this\\ still\\\"not\\\\allowed@example.com"),
+            Arguments.of("i_like_underscore@but_its_not_allowed_in_this_part.example.com"),
+            Arguments.of("QA[icon]CHOCOLATE[icon]@test.com"),
+        )
     }
 
     private fun validEmailArguments(): List<Arguments> {
@@ -32,14 +56,9 @@ class EmailValidatorTest {
             Arguments.of("test/test@test.com"),
             Arguments.of("admin@mailserver1"),
             Arguments.of("example@s.example"),
-            Arguments.of("\" \"@example.org"),
-            Arguments.of("\"john..doe\"@example.org"),
             Arguments.of("mailhost!username@example.org"),
-            Arguments.of("\"very.(),:;<>[]\\\".VERY.\\\"very@\\\\ \\\"very\\\".unusual\"@strange.example.com"),
             Arguments.of("user%example.com@example.org"),
             Arguments.of("user-@example.org"),
-            Arguments.of("postmaster@[123.123.123.123]"),
-            Arguments.of("postmaster@[IPv6:2001:0db8:85a3:0000:0000:8a2e:0370:7334]"),
         )
     }
 }
