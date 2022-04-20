5160199f273926d53dab7c58329fdba70279ed1b
pratheepmm <pratheepm@sahaj.ai>
2022-04-20 12:08:02 +0530

    changed alphaNumeric validator to include only alphabets and numbers

--- src/main/kotlin/validation/implementation/valueValidator/AlphaNumericValidator.kt
+++ src/main/kotlin/validation/implementation/valueValidator/AlphaNumericValidator.kt
@@ -2,6 +2,6 @@ package validation.implementation.valueValidator
 
 class AlphaNumericValidator : ValueTypeValidator {
     override fun validate(value: String, pattern: String?): Boolean {
-        return value.matches("""^[A-Za-z0-9\s]*$""".toRegex())
+        return value.matches("""^[A-Za-z0-9]*$""".toRegex())
     }
 }
