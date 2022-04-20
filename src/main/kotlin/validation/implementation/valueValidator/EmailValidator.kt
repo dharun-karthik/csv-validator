13035040d707bbb7b9279326ca7d28c6f9b7f71d
pratheepmm <pratheepm@sahaj.ai>
2022-04-20 09:17:30 +0530

    changed email validator implementation

--- src/main/kotlin/validation/implementation/valueValidator/EmailValidator.kt
+++ src/main/kotlin/validation/implementation/valueValidator/EmailValidator.kt
@@ -2,7 +2,7 @@ package validation.implementation.valueValidator
 
 class EmailValidator : ValueTypeValidator {
     override fun validate(value: String, pattern: String?): Boolean {
-        return value.matches("""^(.+)@(.+)${'$'}""".toRegex())
+        return value.matches("""^[a-zA-Z0-9_!#${'$'}%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+${'$'}""".toRegex())
     }
 
 }
