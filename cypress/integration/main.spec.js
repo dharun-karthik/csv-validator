c330f9f2cc14555efa96e811ce017170b9bb851a
Shobhit Aryan <shobhitl@sahaj.ai>
2022-04-21 23:52:43 +0530

    Added cypress test to not redirect after clicking upload if no file selected

--- cypress/integration/main.spec.js
+++ cypress/integration/main.spec.js
@@ -5,14 +5,23 @@ describe('cypress connect test', () => {
 })
 
  describe('Uploading file', () => {
-     it('should be able to upload csv file', () => {
+     it('should be able to choose csv file for uploading', () => {
         cy.get('#csv-id').selectFile('cypress/fixtures/correctData.csv')
         cy.get('input[type="file"]').attachFile('correctData.csv');
      })
 
-     it('should be able to upload csv file by drag and drop', () => {
+     it('should be able to drag and drop csv file for uploading', () => {
         cy.get('#csv-id').selectFile('cypress/fixtures/correctData.csv')
         cy.get('input[type="file"]').attachFile('correctData.csv', { subjectType: 'drag-n-drop' });
+     })
+
+     it('should not be able to redirect to addRules page if no file is selcted after clicking upload', () => {
+        cy.on('uncaught:exception', () => {
+            return false;
+          });
+        cy.reload()
+        cy.get('#csv-submit-button').click()
+        cy.url().should('be.equal', 'http://localhost:3000/')
      })
  })
 
