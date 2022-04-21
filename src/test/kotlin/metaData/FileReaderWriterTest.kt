4216c310a865897748ef2396f0182c0b29d8bf49
pratheepmm <pratheepm@sahaj.ai>
2022-04-21 12:19:34 +0530

    added file for testing read in FileReaderWriterTest and renamed reference in FileReaderWriterTest

--- src/test/kotlin/metaData/FileReaderWriterTest.kt
+++ src/test/kotlin/metaData/FileReaderWriterTest.kt
@@ -6,91 +6,12 @@ import org.junit.jupiter.api.Test
 class FileReaderWriterTest {
     @Test
     fun shouldBeAbleToReadRawContent() {
-        val fileReaderWriter = FileReaderWriter("src/test/kotlin/metaDataTestFiles/csv-meta-data-test.json")
+        val fileReaderWriter = FileReaderWriter("src/test/kotlin/metaDataTestFiles/raw/read-raw-content-test.json")
         val expected = """[
   {
     "fieldName": "product id",
     "type": "alphanumeric",
     "length": 5
-  },
-  {
-    "fieldName": "product description",
-    "type": "alphanumeric",
-    "minLength": 7,
-    "maxLength": 20
-  },
-  {
-    "fieldName": "price",
-    "type": "number"
-  },
-  {
-    "fieldName": "export",
-    "type": "alphabets",
-    "values": [
-      "Y",
-      "N"
-    ]
-  },
-  {
-    "fieldName": "country name",
-    "type": "alphabets",
-    "minLength": 3,
-    "dependencies": [
-      {
-        "dependentOn": "export",
-        "expectedDependentFieldValue": "N",
-        "expectedCurrentFieldValue": "null"
-      },
-      {
-        "dependentOn": "export",
-        "expectedDependentFieldValue": "Y",
-        "expectedCurrentFieldValue": "!null"
-      }
-    ]
-  },
-  {
-    "fieldName": "source city",
-    "type": "alphabets",
-    "minLength": 3
-  },
-  {
-    "fieldName": "country code",
-    "type": "number",
-    "maxLength": 3,
-    "dependencies": [
-      {
-        "dependentOn": "country name",
-        "expectedDependentFieldValue": "null",
-        "expectedCurrentFieldValue": "null"
-      },
-      {
-        "dependentOn": "country name",
-        "expectedDependentFieldValue": "!null",
-        "expectedCurrentFieldValue": "!null"
-      }
-    ]
-  },
-  {
-    "fieldName": "source pincode",
-    "type": "number",
-    "length": 6,
-    "values": [
-      "500020",
-      "110001",
-      "560001",
-      "500001",
-      "111045",
-      "230532",
-      "530068",
-      "226020",
-      "533001",
-      "600001",
-      "700001",
-      "212011",
-      "641001",
-      "682001",
-      "444601"
-    ]
   }
 ]"""
 
@@ -101,7 +22,7 @@ class FileReaderWriterTest {
 
     @Test
     fun shouldBeAbleToWriteRawContent(){
-        val fileReaderWriter = FileReaderWriter("src/test/kotlin/metaDataTestFiles/write-content-test.json")
+        val fileReaderWriter = FileReaderWriter("src/test/kotlin/metaDataTestFiles/raw/write-raw-content-test.json")
         val data = """[{"hello":"123"}]"""
 
         fileReaderWriter.writeRawContent(data)
