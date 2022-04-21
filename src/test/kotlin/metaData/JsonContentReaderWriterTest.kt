d9f219470dcfd8f6c5ccd3170816bc2f445ebc44
pratheepmm <pratheepm@sahaj.ai>
2022-04-21 12:13:09 +0530

    written tests for JsonContentReaderWriterTest

---
+++ src/test/kotlin/metaData/JsonContentReaderWriterTest.kt
@@ -0,0 +1,17 @@
+package metaData
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class JsonContentReaderWriterTest {
+    @Test
+    fun shouldBeAbleToReadJsonData() {
+        val jsonContentReaderWriter =
+            JsonContentReaderWriter("src/test/kotlin/metaDataTestFiles/jsonContent/read-json-content-test.json")
+
+        val actual = jsonContentReaderWriter.readJsonData()
+        val expected = """[{"hello":"hi","Product Description":"Table chair"}]"""
+
+        assertEquals(expected,actual)
+    }
+}
