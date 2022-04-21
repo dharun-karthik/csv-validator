808b87f83f22a864e5c21084e48d1356426a890c
pratheepmm <pratheepm@sahaj.ai>
2022-04-21 16:26:53 +0530

    added more test case

--- src/test/kotlin/routeHandler/get/CsvValidatorTest.kt
+++ src/test/kotlin/routeHandler/get/CsvValidatorTest.kt
@@ -22,6 +22,21 @@ class CsvValidatorTest {
         assertEquals(expected, actual)
     }
 
+    @Test
+    fun shouldReturnErrorValuesWhenTheValidationFails(){
+        val configFileReaderWriter =
+            ConfigFileReaderWriter("src/test/kotlin/metaDataTestFiles/csvValidation/csv-config-test.json")
+        val jsonContentReaderWriter =
+            JsonContentReaderWriter("src/test/kotlin/metaDataTestFiles/csvValidation/content-with-error-test.json")
+        val csvValidator = CsvValidator(configFileReaderWriter, jsonContentReaderWriter)
+
+        val actual = csvValidator.handleCsv()
+        val content = """[{"2":{"Length Error in":["product description : Table"],"Value Not Found":["source pincode : 560002"]}}]"""
+        val expected = getBodyWithSuccessHeader(content)
+
+        assertEquals(expected, actual)
+    }
+
     private fun getBodyWithSuccessHeader(body: String): String {
         val lineBreak = System.lineSeparator()
         return """HTTP/1.1 200 OK
