dac0200fff2c6748a3780eeb1081a1a67dcb6287
DharunKarthik <dharunk@sahaj.ai>
2022-04-22 05:47:54 +0530

    Bug Fix : Empty values are changed to null for last column

--- src/main/public/js/Convert.js
+++ src/main/public/js/Convert.js
@@ -4,9 +4,9 @@ function csvToJson(lines) {
     const headers = headerWithoutTrim.map(element => element.trim().toLowerCase())
     for (let i = 1; i < lines.length; i++) {
         const obj = {};
-        const currentLine = lines[i].split(",");
+        const currentLine = (lines[i].split(","));
         for (let j = 0; j < headers.length; j++) {
-            if (currentLine[j] == "") {
+            if (currentLine[j] == "" || currentLine[j] == "\r") {
                 obj[headers[j]] = "null"
                 continue;
             }
