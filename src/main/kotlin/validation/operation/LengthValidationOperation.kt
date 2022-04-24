666b3826bdc0f541897e99eabe4e937f098f15a5
pratheepmm <pratheepm@sahaj.ai>
2022-04-24 11:26:01 +0530

    refactored the implementation of calling length validations (min, max , fixed)

--- src/main/kotlin/validation/operation/LengthValidationOperation.kt
+++ src/main/kotlin/validation/operation/LengthValidationOperation.kt
@@ -4,7 +4,6 @@ import lengthValidator.*
 import lengthValidator.LengthType.*
 import metaData.template.JsonMetaDataTemplate
 import org.json.JSONObject
-import validation.implementation.LengthValidation
 
 class LengthValidationOperation : ValidationOperation {
     private val lengthTypeMap: Map<LengthType, LengthTypeValidator> = mapOf(
@@ -16,41 +15,22 @@ class LengthValidationOperation : ValidationOperation {
     override fun validate(
         metaDataField: JsonMetaDataTemplate, currentFieldValue: String, currentRow: JSONObject?
     ): String? {
-        val lengthValidation = LengthValidation()
         if (isFieldIsNull(currentFieldValue)) {
             return null
         }
-        return lengthCheck(currentFieldValue, metaDataField, lengthValidation)
+        return lengthCheck(currentFieldValue, metaDataField)
     }
 
     private fun lengthCheck(
-        currentFieldValue: String, metaDataField: JsonMetaDataTemplate, lengthValidation: LengthValidation
+        currentFieldValue: String, metaDataField: JsonMetaDataTemplate
     ): String? {
-        val isValid = (checkFixedLength(currentFieldValue, metaDataField, lengthValidation) && checkMinLength(
-            currentFieldValue, metaDataField, lengthValidation
-        ) && checkMaxLength(currentFieldValue, metaDataField, lengthValidation))
-
-        if (!isValid) {
-            return "Length Error in"
+        lengthTypeMap.forEach { entry ->
+            val lengthTypeValidation = entry.value
+            val field = lengthTypeValidation.getAppropriateLengthRestriction(metaDataField)
+            if (lengthTypeValidation.validateLengthType(currentFieldValue, field)) {
+                return "Length Error in"
+            }
         }
         return null
     }
-
-    private fun checkMaxLength(
-        currentFieldValue: String, metaDataField: JsonMetaDataTemplate, lengthValidation: LengthValidation
-    ) = lengthTypeMap[MAX_LENGTH]!!.validateLengthType(
-        currentFieldValue, metaDataField.maxLength?.toInt(), lengthValidation
-    )
-
-    private fun checkMinLength(
-        currentFieldValue: String, metaDataField: JsonMetaDataTemplate, lengthValidation: LengthValidation
-    ) = lengthTypeMap[MIN_LENGTH]!!.validateLengthType(
-        currentFieldValue, metaDataField.minLength?.toInt(), lengthValidation
-    )
-
-    private fun checkFixedLength(
-        currentFieldValue: String, metaDataField: JsonMetaDataTemplate, lengthValidation: LengthValidation
-    ) = lengthTypeMap[FIXED_LENGTH]!!.validateLengthType(
-        currentFieldValue, metaDataField.length?.toInt(), lengthValidation
-    )
 }
