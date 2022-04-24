53d54b30888da4670b29e984cb5910ae4dd69bab
pratheepmm <pratheepm@sahaj.ai>
2022-04-24 20:13:07 +0530

    added tests for restricted input validation operation

---
+++ src/test/kotlin/validation/operation/RestrictedInputValidationOperationTest.kt
@@ -0,0 +1,29 @@
+package validation.operation
+
+import metaData.template.JsonMetaDataTemplate
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNull
+import org.junit.jupiter.api.Test
+
+internal class RestrictedInputValidationOperationTest {
+    @Test
+    fun shouldGetNullWhenFieldValueIsNull() {
+        val restrictedInputValidationOperation = RestrictedInputValidationOperation()
+        val metaDataField = JsonMetaDataTemplate("test", "number")
+
+        val actual = restrictedInputValidationOperation.validate(metaDataField, "null", "test")
+
+        assertNull(actual)
+    }
+
+    @Test
+    fun shouldGetAppropriateErrorMessage() {
+        val restrictedInputValidationOperation = RestrictedInputValidationOperation()
+        val metaDataField = JsonMetaDataTemplate("test", "number", values = listOf("123"))
+
+        val actual = restrictedInputValidationOperation.validate(metaDataField, "12", "test")
+        val expected = "Value Not Found test : 12"
+
+        assertEquals(expected, actual)
+    }
+}
