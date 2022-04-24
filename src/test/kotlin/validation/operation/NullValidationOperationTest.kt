4a1014a1d12508eaac53220fb2c82e97088bb8df
pratheepmm <pratheepm@sahaj.ai>
2022-04-24 20:23:40 +0530

    added tests for null validation operation

---
+++ src/test/kotlin/validation/operation/NullValidationOperationTest.kt
@@ -0,0 +1,30 @@
+package validation.operation
+
+import metaData.template.JsonMetaDataTemplate
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class NullValidationOperationTest {
+
+    @Test
+    fun shouldGetErrorWhenFieldValueIsNullAndNullIsNotAllowed() {
+        val nullValidationOperation = NullValidationOperation()
+        val metaDataField = JsonMetaDataTemplate("test", "number")
+
+        val actual = nullValidationOperation.validate(metaDataField, "null", "test")
+
+        assertNull(actual)
+    }
+
+    @Test
+    fun shouldGetNullWhenFieldValueIsNullAndNullIsAllowed() {
+        val nullValidationOperation = NullValidationOperation()
+        val metaDataField = JsonMetaDataTemplate("test", "number", isNullAllowed = "No")
+
+        val actual = nullValidationOperation.validate(metaDataField, "null", "test")
+        val expected = "Empty Value not allowed in test"
+
+        assertEquals(expected, actual)
+    }
+}
