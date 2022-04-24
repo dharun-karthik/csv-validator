c90b2edc1ffa76974f341241e58e09d21f17d820
amitkhirdekar <amitk@sahaj.ai>
2022-04-24 19:54:08 +0530

    Added negative constraint for length fields

--- src/main/public/js/app.js
+++ src/main/public/js/app.js
@@ -314,15 +314,15 @@ function addNewField() {
     <div class="row1" id="row${numberOfFields}">
         <div id="min-length">
             <label for="min-len${numberOfFields}">Min-length: </label>
-            <input type="number" name="minLength" id="min-len${numberOfFields}" class="inputs" onchange="onChangeHandler(event)">
+            <input type="number" name="minLength" id="min-len${numberOfFields}" class="inputs" onchange="onChangeHandler(event)" min="0" oninput="validity.valid||(value='');">
         </div>
         <div id="max-length">
             <label for="max-len${numberOfFields}">Max-length: </label>
-            <input type="number" name="maxLength" id="max-len${numberOfFields}" class="inputs" onchange="onChangeHandler(event)">
+            <input type="number" name="maxLength" id="max-len${numberOfFields}" class="inputs" onchange="onChangeHandler(event)" min="0" oninput="validity.valid||(value='');">
         </div>
         <div id="fixed-length">
             <label for="fixed-len${numberOfFields}">Fixed-length: </label>
-            <input type="number" name="length" id="fixed-len${numberOfFields}" class="inputs" onchange="onChangeHandler(event)">
+            <input type="number" name="length" id="fixed-len${numberOfFields}" class="inputs" onchange="onChangeHandler(event)" min="0" oninput="validity.valid||(value='');">
         </div>
     </div>
     <div class="row1">
