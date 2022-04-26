a2fee9031542d4cbc75f418e37c3838cdc1a7f5a
pratheepmm <pratheepm@sahaj.ai>
2022-04-26 15:24:55 +0530

    added dependency opearation test

---
+++ src/test/kotlin/validation/operation/DependencyValidationOperationTest.kt
@@ -0,0 +1,36 @@
+package validation.operation
+
+import metaData.template.DependencyTemplate
+import metaData.template.JsonMetaDataTemplate
+import org.json.JSONObject
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNull
+import org.junit.jupiter.api.Test
+
+class DependencyValidationOperationTest {
+
+    @Test
+    fun shouldGetErrorWhenDependencyIsNotMet() {
+        val dependencyValidation = DependencyValidationOperation()
+        val dependency = DependencyTemplate("parent", "null", "null")
+        val metaDataField = JsonMetaDataTemplate("test", "alphabets", dependencies = listOf(dependency))
+        val currentRow = JSONObject("""{"test":"hello","parent":"null"}""")
+
+        val actual = dependencyValidation.validate(metaDataField, "hello", "test", currentRow)
+        val expected = "parent is null but test is hello"
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun shouldGetNullWhenDependencyConditionIsMet() {
+        val dependencyValidation = DependencyValidationOperation()
+        val dependency = DependencyTemplate("parent", "null", "null")
+        val metaDataField = JsonMetaDataTemplate("test", "alphabets", dependencies = listOf(dependency))
+        val currentRow = JSONObject("""{"test":"null","parent":"null"}""")
+
+        val actual = dependencyValidation.validate(metaDataField, "null", "test", currentRow)
+
+        assertNull(actual)
+    }
+}
