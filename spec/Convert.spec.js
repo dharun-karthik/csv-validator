e27f64c6303255fef5a1c2357484b1a0f2215da6
Shobhit Aryan <shobhitl@sahaj.ai>
2022-04-26 12:39:34 +0530

    Added jasmine test to validate conversion from csv having commas inside double quotes to JSON

--- spec/Convert.spec.js
+++ spec/Convert.spec.js
@@ -4,9 +4,19 @@ const convert = rewire("../src/main/public/js/Convert.js");
 
 describe("convert", () => {
     it("should convert csv to json", () => {
-        let lines = ["a,b,c", "1,xyz,1.2", "2,pqr,2.4"]
+        let lines = `a,b,c\n1,xyz,1.2\n2,pqr,2.4`
         let csvToJson = convert.__get__("csvToJson")
         let expected = [{a: '1', b: 'xyz', c: '1.2'}, {a: '2', b: 'pqr', c: '2.4'}]
+
+        let actual = csvToJson(lines);
+
+        expect(actual).toEqual(expected)
+    })
+
+    it("should convert csv having commas inside double quotes to json", () => {
+        let lines = `a,b,c\n1,"xyz,lmn",1.2\n2,"pqr,jhk","2.4,a"`
+        let csvToJson = convert.__get__("csvToJson")
+        let expected = [{a: '1', b: 'xyz,lmn', c: '1.2'}, {a: '2', b: 'pqr,jhk', c: '2.4,a'}]
 
         let actual = csvToJson(lines);
 
