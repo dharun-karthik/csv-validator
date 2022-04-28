7b264a3f26f2beb8280b51ca6d7e88b7981bcdb9
pratheepmm <pratheepm@sahaj.ai>
2022-04-29 00:09:05 +0530

    added min max length validation tests

---
+++ src/test/kotlin/validation/jsonConfig/MaxMinValidatorTest.kt
@@ -0,0 +1,53 @@
+package validation.jsonConfig
+
+import metaData.template.JsonConfigTemplate
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class MaxMinValidatorTest {
+
+    @Test
+    fun shouldGiveErrorWhenMinIsGreaterThanMaxLength() {
+        val maxMinValidator = MaxMinValidator()
+        val jsonConfig= JsonConfigTemplate("sample","number", minLength = "5", maxLength = "3")
+
+        val expected = listOf("Max length : 3 should be greater than min length : 5")
+        val actual = maxMinValidator.validate(jsonConfig)
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun shouldNotGetErrorWhenMaxLengthIsNotProvided() {
+        val maxMinValidator = MaxMinValidator()
+        val jsonConfig= JsonConfigTemplate("sample","number", minLength = "5")
+
+        val expected = listOf<String>()
+        val actual = maxMinValidator.validate(jsonConfig)
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun shouldNotGetErrorWhenMinLengthIsNotProvided() {
+        val maxMinValidator = MaxMinValidator()
+        val jsonConfig= JsonConfigTemplate("sample","number", maxLength = "5")
+
+        val expected = listOf<String>()
+        val actual = maxMinValidator.validate(jsonConfig)
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun shouldNotGetErrorWhenBothMinOrMaxLengthIsNotProvided() {
+        val maxMinValidator = MaxMinValidator()
+        val jsonConfig= JsonConfigTemplate("sample","number")
+
+        val expected = listOf<String>()
+        val actual = maxMinValidator.validate(jsonConfig)
+
+        assertEquals(expected, actual)
+    }
+}
