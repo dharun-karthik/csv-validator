441ab073bff093662399dd7f03e9fcf4e9016521
pratheepmm <pratheepm@sahaj.ai>
2022-04-29 00:09:24 +0530

    implemented min max validator functionality

--- src/main/kotlin/validation/jsonConfig/MaxMinValidator.kt
+++ src/main/kotlin/validation/jsonConfig/MaxMinValidator.kt
@@ -4,6 +4,10 @@ import metaData.template.JsonConfigTemplate
 
 class MaxMinValidator: ConfigValidator {
     override fun validate(jsonField: JsonConfigTemplate): List<String> {
-        TODO("Not yet implemented")
+        val error = mutableListOf<String>()
+        if(jsonField.minLength !=null && jsonField.maxLength!=null && jsonField.minLength > jsonField.maxLength){
+            error.add("Max length : ${jsonField.maxLength} should be greater than min length : ${jsonField.minLength}")
+        }
+        return error
     }
 }
