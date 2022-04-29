47ce7f485b1f7bae982208f5e6dc1d4ea69fb0f1
pratheepmm <pratheepm@sahaj.ai>
2022-04-29 21:39:37 +0530

    written more tests for config validation

--- src/test/kotlin/routeHandler/post/ConfigWriterTest.kt
+++ src/test/kotlin/routeHandler/post/ConfigWriterTest.kt
@@ -4,6 +4,7 @@ import fakeStreams.FakeInputStreamProvider
 import metaData.ConfigFileReaderWriter
 import metaData.template.JsonConfigTemplate
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 class ConfigWriterTest {
@@ -42,5 +43,81 @@ class ConfigWriterTest {
             maxLength = 20
         )
         assertEquals(expected, fields)
+    }
+
+    @Test
+    fun shouldNotWriteConfigIfValidationFails() {
+        val configFileReaderWriter =
+            ConfigFileReaderWriter("src/test/kotlin/metaDataTestFiles/configContent/should-not-write-config-test.json")
+        val post = ConfigWriter(configFileReaderWriter)
+        val data = """[
+  {
+    "type": "AlphaNumeric",
+    "length": 5
+  },{
+    "fieldName": "Product Description",
+    "minLength": 20,
+    "maxLength": 7
+  }]"""
+
+        val fakeInputStreamProvider = FakeInputStreamProvider(data)
+        val request = """
+            Content-Length: ${data.length}
+            
+        """
+        post.uploadValidConfigJson(request, fakeInputStreamProvider)
+        val fields = post.configFileReaderWriter.readFields()
+
+        assertTrue(fields.isEmpty())
+    }
+
+    @Test
+    fun shouldGetResponseIfValidationFails() {
+        val configFileReaderWriter =
+            ConfigFileReaderWriter("src/test/kotlin/metaDataTestFiles/configContent/should-not-write-config-test.json")
+        val post = ConfigWriter(configFileReaderWriter)
+        val data = """[
+  {
+    "type": "AlphaNumeric",
+    "length": 5
+  },{
+    "fieldName": "Product Description",
+    "minLength": 20,
+    "maxLength": 7
+  }]"""
+
+        val fakeInputStreamProvider = FakeInputStreamProvider(data)
+        val request = """
+            Content-Length: ${data.length}
+            
+        """
+        val content =
+            """[{"1":[{"Field errors":["Field 'fieldName' should be provided"]}]},{"2":[{"Field errors":["Field 'type' should be provided","Max length : 7 should be greater than min length : 20"]}]}]"""
+        val expected = """HTTP/1.1 400 Bad Request
+            |Content-Type: application/json; charset=utf-8
+            |Content-Length: ${content.length}
+            |
+            |$content""".trimMargin()
+        val actual = post.uploadValidConfigJson(request, fakeInputStreamProvider)
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun shouldNotWriteConfigIfValuesAreEmpty() {
+        val configFileReaderWriter =
+            ConfigFileReaderWriter("src/test/kotlin/metaDataTestFiles/configContent/empty-config-write-test.json")
+        val post = ConfigWriter(configFileReaderWriter)
+        val data = """[]"""
+
+        val fakeInputStreamProvider = FakeInputStreamProvider(data)
+        val request = """
+            Content-Length: ${data.length}
+            
+        """
+        post.uploadValidConfigJson(request, fakeInputStreamProvider)
+        val fields = post.configFileReaderWriter.readFields()
+
+        assertTrue(fields.isEmpty())
     }
 }
