8177afcc9fe3d631f81079446da3e5f008c7ec49
Shobhit Aryan <shobhitl@sahaj.ai>
2022-05-02 11:38:16 +0530

    Added parameterized test to validate expected format in error message for time and datetime

--- src/test/kotlin/validation/operation/TypeValidatorOperationTest.kt
+++ src/test/kotlin/validation/operation/TypeValidatorOperationTest.kt
@@ -4,7 +4,12 @@ import metaData.template.JsonConfigTemplate
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 internal class TypeValidatorOperationTest {
 
     @Test
@@ -28,14 +33,28 @@ internal class TypeValidatorOperationTest {
         assertEquals(expected, actual)
     }
 
-    @Test
-    fun shouldGetExpectedFormatInErrorMessageForDate() {
+    @ParameterizedTest()
+    @MethodSource("typeAndPatternArguments")
+    fun shouldGetExpectedFormatInErrorMessageForDate(
+        type: String,
+        pattern: String,
+        currentFieldValue: String,
+        expectedPattern: String
+    ) {
         val typeValidation = TypeValidationOperation()
-        val metaDataField = JsonConfigTemplate("test", "date", pattern = "uuuu/MM/dd")
+        val metaDataField = JsonConfigTemplate("test", type, pattern = pattern)
 
-        val actual = typeValidation.validate(metaDataField, "02/2022/05", "test")
-        val expected = "Incorrect format of 'date' in test : 02/2022/05, expected format : yyyy/MM/dd"
+        val actual = typeValidation.validate(metaDataField, currentFieldValue, "test")
+        val expected = "Incorrect format of '$type' in test : $currentFieldValue, expected format : $expectedPattern"
 
         assertEquals(expected, actual)
+    }
+
+    private fun typeAndPatternArguments(): List<Arguments> {
+        return listOf(
+            Arguments.of("date", "uuuu/MM/dd", "02/2022/05", "yyyy/MM/dd"),
+            Arguments.of("time","HH:MM:ss", "abc", "HH:MM:ss"),
+            Arguments.of("date-time", "HH:mm:ss?dd/uuuu/MM", "2022-05-02T12:10:09")
+        )
     }
 }
