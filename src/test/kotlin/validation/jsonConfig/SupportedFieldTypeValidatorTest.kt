5825ff6cdce61a6c776543256132e81906aef259
Shobhit Aryan <shobhitl@sahaj.ai>
2022-05-03 11:55:49 +0530

    Added parameterized test to not throw error for supported field types

--- src/test/kotlin/validation/jsonConfig/SupportedFieldTypeValidatorTest.kt
+++ src/test/kotlin/validation/jsonConfig/SupportedFieldTypeValidatorTest.kt
@@ -3,7 +3,12 @@ package validation.jsonConfig
 import metaData.template.JsonConfigTemplate
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 internal class SupportedFieldTypeValidatorTest {
     @Test
     fun shouldReturnErrorForFieldTypeNotSupported() {
@@ -12,6 +17,32 @@ internal class SupportedFieldTypeValidatorTest {
         val expected = listOf("Field type abc is not supported")
 
         val actual = supportedFieldTypeValidator.validate(jsonConfig)
+
         assertEquals(expected, actual)
+    }
+
+    @ParameterizedTest
+    @MethodSource("supportedFieldTypeArguments")
+    fun shouldNotReturnErrorForSupportedFieldType(type : String) {
+        val supportedFieldTypeValidator = SupportedFieldTypeValidator()
+        val jsonConfig = JsonConfigTemplate("test", type)
+        val expected = listOf<String>()
+
+        val actual = supportedFieldTypeValidator.validate(jsonConfig)
+
+        assertEquals(expected, actual)
+    }
+
+    private fun supportedFieldTypeArguments() : List<Arguments> {
+        return listOf<Arguments>(
+            Arguments.of("number"),
+            Arguments.of("alphanumeric"),
+            Arguments.of("alphabets"),
+            Arguments.of("date"),
+            Arguments.of("time"),
+            Arguments.of("datetime"),
+            Arguments.of("email"),
+            Arguments.of("text")
+        )
     }
 }
