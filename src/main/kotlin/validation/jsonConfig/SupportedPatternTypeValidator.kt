f47613fa88f2bb5511df23f94e1066b1dc3700f7
Shobhit Aryan <shobhitl@sahaj.ai>
2022-05-03 13:06:40 +0530

    Added patterns for time and datetime

--- src/main/kotlin/validation/jsonConfig/SupportedPatternTypeValidator.kt
+++ src/main/kotlin/validation/jsonConfig/SupportedPatternTypeValidator.kt
@@ -2,7 +2,7 @@ package validation.jsonConfig
 
 import metaData.template.JsonConfigTemplate
 
-class SupportedPatternTypeValidator : ConfigValidator{
+class SupportedPatternTypeValidator : ConfigValidator {
 
     private val supportedPatternTypeList = listOf(
         "dd/MM/uuuu",
@@ -16,12 +16,35 @@ class SupportedPatternTypeValidator : ConfigValidator{
         "MM-dd-uuuu",
         "MM-uuuu-dd",
         "uuuu-dd-MM",
-        "uuuu-MM-dd"
+        "uuuu-MM-dd",
+        "HH:mm:ss",
+        "HH:ss:mm",
+        "ss:HH:mm",
+        "HH:ss:mm:SSS",
+        "hh:ss:mm a",
+        "hh:ss:mma",
+        "ahh:ss:mm",
+        "hh:ass:mm",
+        "hh:ass:mm:SSS",
+        "HH:mm:ss dd/MM/uuuu",
+        "HH:mm:ss?dd/uuuu/MM",
+        "HH:ss:mm:dd:MM:uuuu",
+        "ss:HH:mm/uuuu/MM/dd",
+        "HH:ss:mm-dd-uuuu-MM",
+        "HH:ss:mm:SSS dd MM uuuu",
+        "hh:ss:mm a,uuuu-MM-dd",
+        "hh:ss:mma+dd/MM/uuuu",
+        "ahh:ss:mm () dd/MM/uuuu",
+        "dd/MM/uuuu hh:ass:mm",
+        "dd/MM/uuuu == hh:ass:mm:SSS",
+        "dd MM uuuu hh:ass:mm",
+        "uuuu-MM-dd't'HH:mm:ss",
+        "uuuu-MM-dd't'HH:mm:ss.SSS'z'",
     )
 
     override fun validate(jsonField: JsonConfigTemplate): List<String> {
         val genericValidator = GenericValidator()
-        if(jsonField.pattern == null) {
+        if (jsonField.pattern == null) {
             return listOf()
         }
         return genericValidator.validate(jsonField.pattern, supportedPatternTypeList)
