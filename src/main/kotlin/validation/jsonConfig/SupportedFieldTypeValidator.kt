7a263f42b90f37971fa909043adb263276bca4da
Shobhit Aryan <shobhitl@sahaj.ai>
2022-05-03 11:45:54 +0530

    Added SupportedFieldTypeValidator class to validate only supported field type

---
+++ src/main/kotlin/validation/jsonConfig/SupportedFieldTypeValidator.kt
@@ -0,0 +1,14 @@
+package validation.jsonConfig
+
+import metaData.template.JsonConfigTemplate
+
+class SupportedFieldTypeValidator : ConfigValidator {
+
+    private val supportedFieldTypeList = listOf("number", "alphanumeric", "alphabets", "date", "time", "datetime", "email", "text")
+
+    override fun validate(jsonField: JsonConfigTemplate): List<String> {
+        val genericValidator = GenericValidator()
+        return genericValidator.validate(jsonField.type, supportedFieldTypeList)
+    }
+
+}
