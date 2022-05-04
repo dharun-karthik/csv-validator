bc8cbbc815f4a2e77bf60b8e2d4e49f41be3d658
pratheepmm <pratheepm@sahaj.ai>
2022-05-04 16:00:41 +0530

    changed DBConnection implementation

--- src/main/kotlin/db/DBConnection.kt
+++ src/main/kotlin/db/DBConnection.kt
@@ -2,17 +2,18 @@ package db
 
 import java.sql.Connection
 import java.sql.DriverManager
-import java.sql.Statement
 
 object DBConnection {
-    val connection: Connection
+    private var connection: Connection? = null
 
-    init {
-        val url = "jdbc:postgresql://localhost:5432/csv_validator"
+    fun initialise(url: String = "jdbc:postgresql://localhost:5432/csv_validator") {
         connection = DriverManager.getConnection(url, "db_user", "database-user")
     }
 
-    fun createStatement(): Statement {
-        return connection.createStatement()
+    fun getDBConnection(): Connection {
+        if (connection == null) {
+            initialise()
+        }
+        return connection!!
     }
 }
