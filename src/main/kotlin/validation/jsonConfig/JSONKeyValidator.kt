391fed8a8db6569f4e14599e2891e35425fe12c8
Shobhit Aryan <shobhitl@sahaj.ai>
2022-05-04 12:15:21 +0530

    Added functionality to validate keys of a json object

--- src/main/kotlin/validation/jsonConfig/JSONKeyValidator.kt
+++ src/main/kotlin/validation/jsonConfig/JSONKeyValidator.kt
@@ -1,4 +1,28 @@
 package validation.jsonConfig
 
+import org.json.JSONObject
+
 class JSONKeyValidator {
+    fun validateKey(jsonField: JSONObject): List<String> {
+        val validKeyList = listOf(
+            "fieldName",
+            "type",
+            "isNullAllowed",
+            "pattern",
+            "length",
+            "minLength",
+            "maxLength",
+            "dependencies",
+            "values"
+        )
+        val keyErrorList = mutableListOf<String>()
+        val keys = jsonField.keys()
+        while (keys.hasNext()) {
+            val key = keys.next()
+            if (key !in validKeyList) {
+                keyErrorList.add("$key is not a valid key")
+            }
+        }
+        return keyErrorList
+    }
 }
