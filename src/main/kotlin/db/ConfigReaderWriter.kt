f3d4fd392efd00b70b327ada212a45f071bc1d39
pratheepmm <pratheepm@sahaj.ai>
2022-05-04 16:56:42 +0530

    implemented reading config from database

--- src/main/kotlin/db/ConfigReaderWriter.kt
+++ src/main/kotlin/db/ConfigReaderWriter.kt
@@ -4,26 +4,101 @@ import com.google.gson.Gson
 import metaData.template.DependencyTemplate
 import metaData.template.JsonConfigTemplate
 import java.sql.PreparedStatement
+import java.sql.ResultSet
 import java.sql.Types
 
 class ConfigReaderWriter {
     private val gson = Gson()
 
-    fun readConfig(configName: String): Array<JsonConfigTemplate> {
-        val statement = DBConnection.createStatement()
-        val result = statement.executeQuery("SELECT * FROM fields")
+    fun readConfig(configName: String): MutableList<JsonConfigTemplate> {
+        val query = """
+            SELECT config_name,
+                   field_id,
+                   field_name,
+                   field_type,
+                   is_null_allowed,
+                   pattern,
+                   fixed_length,
+                   min_length,
+                   max_length
+            FROM csv_configuration
+                     INNER JOIN fields f on csv_configuration.config_id = f.config_id
+            where config_name LIKE ?;
+        """
+
+        val preparedStatement = DBConnection.getDBConnection().prepareStatement(query)
+        preparedStatement.setString(1, configName)
+        val result = preparedStatement.executeQuery()
+        val finalConfig = mutableListOf<JsonConfigTemplate>()
         while (result.next()) {
-            println(result.getString("field_name"))
+            finalConfig.add(getJsonConfig(result))
         }
-        val data = ""
-        return gson.fromJson(data, Array<JsonConfigTemplate>::class.java) ?: return arrayOf()
+        return finalConfig
+    }
+
+    private fun getJsonConfig(result: ResultSet): JsonConfigTemplate {
+        val fieldId = result.getInt("field_id")
+        val fieldName = result.getString("field_name")
+        val fieldType = result.getString("field_type")
+        val isNullAllowed = result.getString("is_null_allowed")
+        val pattern = result.getString("pattern")
+        val fixedLength = result.getInt("fixed_length")
+        val minLength = result.getInt("min_length")
+        val maxLength = result.getInt("max_length")
+        val values = getValues(fieldId)
+        val dependencies = getDependencies(fieldId)
+        return JsonConfigTemplate(
+            fieldName = fieldName,
+            type = fieldType,
+            isNullAllowed = isNullAllowed,
+            pattern = pattern,
+            length = fixedLength,
+            minLength = minLength,
+            maxLength = maxLength,
+            values = values,
+            dependencies = dependencies
+        )
+    }
+
+    private fun getDependencies(fieldId: Int): List<DependencyTemplate>? {
+        val query =
+            "SELECT dependent_on,expected_dependent_value,expected_current_value from dependencies WHERE field_id = ?"
+        val preparedStatement = DBConnection.getDBConnection().prepareStatement(query)
+        preparedStatement.setInt(1, fieldId)
+        val result = preparedStatement.executeQuery()
+        val dependencies = mutableListOf<DependencyTemplate>()
+        while (result.next()) {
+            val dependentOn = result.getString("dependent_on")
+            val expectedDependentValue = result.getString("expected_dependent_value")
+            val expectedCurrentValue = result.getString("expected_current_value")
+            dependencies.add(DependencyTemplate(dependentOn, expectedDependentValue, expectedCurrentValue))
+        }
+        if (dependencies.isEmpty()) {
+            return null
+        }
+        return dependencies
+    }
+
+    private fun getValues(fieldId: Int): List<String>? {
+        val query = "SELECT value_name from values WHERE field_id = ?"
+        val preparedStatement = DBConnection.getDBConnection().prepareStatement(query)
+        preparedStatement.setInt(1, fieldId)
+        val result = preparedStatement.executeQuery()
+        val values = mutableListOf<String>()
+        while (result.next()) {
+            values.add(result.getString("value_name"))
+        }
+        if (values.isEmpty()) {
+            return null
+        }
+        return values
     }
 
     fun addConfig(configName: String, jsonData: Array<JsonConfigTemplate>) {
         val queryTemplate = "INSERT INTO csv_configuration(config_name) VALUES(?) RETURNING config_id"
-        val statement = DBConnection.connection.prepareStatement(queryTemplate)
-        statement.setString(1, configName)
-        val result = statement.executeQuery()
+        val preparedStatement = DBConnection.getDBConnection().prepareStatement(queryTemplate)
+        preparedStatement.setString(1, configName)
+        val result = preparedStatement.executeQuery()
         if (result.next()) {
             for (data in jsonData) {
                 val configId = result.getInt("config_id")
@@ -35,9 +110,9 @@ class ConfigReaderWriter {
     private fun insertFields(configId: Int, jsonData: JsonConfigTemplate) {
         val queryTemplate =
             "INSERT INTO fields(config_id, field_name, field_type, is_null_allowed, pattern, fixed_length, min_length, max_length) VALUES(?,?,?,?,?,?,?,?) RETURNING field_id"
-        val statement = DBConnection.connection.prepareStatement(queryTemplate)
-        setQueryFields(statement, configId, jsonData)
-        val result = statement.executeQuery()
+        val preparedStatement = DBConnection.getDBConnection().prepareStatement(queryTemplate)
+        setQueryFields(preparedStatement, configId, jsonData)
+        val result = preparedStatement.executeQuery()
         if (result.next()) {
             val fieldId = result.getInt("field_id")
             insertValues(fieldId, jsonData.values)
@@ -54,12 +129,12 @@ class ConfigReaderWriter {
     private fun insertDependency(fieldId: Int, dependency: DependencyTemplate) {
         val queryTemplate =
             "INSERT INTO dependencies(field_id, dependent_on, expected_dependent_value, expected_current_value) VALUES(?,?,?,?)"
-        val statement = DBConnection.connection.prepareStatement(queryTemplate)
-        statement.setInt(1, fieldId)
-        statement.setString(2, dependency.dependentOn)
-        statement.setString(3, dependency.expectedDependentFieldValue)
-        statement.setString(4, dependency.expectedCurrentFieldValue)
-         statement.executeUpdate()
+        val preparedStatement = DBConnection.getDBConnection().prepareStatement(queryTemplate)
+        preparedStatement.setInt(1, fieldId)
+        preparedStatement.setString(2, dependency.dependentOn)
+        preparedStatement.setString(3, dependency.expectedDependentFieldValue)
+        preparedStatement.setString(4, dependency.expectedCurrentFieldValue)
+        preparedStatement.executeUpdate()
     }
 
     private fun insertValues(fieldId: Int, values: List<String>?) {
@@ -70,36 +145,32 @@ class ConfigReaderWriter {
 
     private fun insertValue(fieldId: Int, value: String) {
         val queryTemplate = "INSERT INTO values(field_id, value_name) VALUES(?,?)"
-        val statement = DBConnection.connection.prepareStatement(queryTemplate)
-        statement.setInt(1, fieldId)
-        statement.setString(2, value)
-        statement.executeUpdate()
+        val preparedStatement = DBConnection.getDBConnection().prepareStatement(queryTemplate)
+        preparedStatement.setInt(1, fieldId)
+        preparedStatement.setString(2, value)
+        preparedStatement.executeUpdate()
     }
 
     private fun setQueryFields(
-        statement: PreparedStatement,
+        preparedStatement: PreparedStatement,
         configId: Int,
         jsonData: JsonConfigTemplate
     ) {
-        statement.setInt(1, configId)
-        statement.setString(2, jsonData.fieldName)
-        statement.setString(3, jsonData.type)
-        statement.setString(4, jsonData.isNullAllowed)
-        statement.setString(5, jsonData.pattern)
-        if (jsonData.length == null) {
-            statement.setNull(6, Types.INTEGER)
+        preparedStatement.setInt(1, configId)
+        preparedStatement.setString(2, jsonData.fieldName)
+        preparedStatement.setString(3, jsonData.type)
+        preparedStatement.setString(4, jsonData.isNullAllowed)
+        preparedStatement.setString(5, jsonData.pattern)
+        setInt(jsonData.length, 6, preparedStatement)
+        setInt(jsonData.minLength, 7, preparedStatement)
+        setInt(jsonData.maxLength, 8, preparedStatement)
+    }
+
+    private fun setInt(value: Int?, index: Int, statement: PreparedStatement) {
+        if (value == null) {
+            statement.setNull(index, Types.INTEGER)
         } else {
-            statement.setInt(6, jsonData.length)
-        }
-        if (jsonData.minLength == null) {
-            statement.setNull(7, Types.INTEGER)
-        } else {
-            statement.setInt(7, jsonData.minLength)
-        }
-        if (jsonData.maxLength == null) {
-            statement.setNull(8, Types.INTEGER)
-        } else {
-            statement.setInt(8, jsonData.maxLength)
+            statement.setInt(index, value)
         }
     }
 }
