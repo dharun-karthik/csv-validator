ecc47a9f1128bc88a41e381cc3aeb1f5a746ec68
DharunKarthik <dharunk@sahaj.ai>
2022-05-05 10:06:19 +0530

    Used H2db for testing ConfigReadWriter

--- src/test/kotlin/db/ConfigReaderWriterTest.kt
+++ src/test/kotlin/db/ConfigReaderWriterTest.kt
@@ -35,46 +35,50 @@ internal class ConfigReaderWriterTest {
         fun shouldWriteTheDataWithFieldNameAndTypeOnly() {
             val configName = "nameAndType"
             val jsonData = createArrayOfJsonTemplate()
-            val expected = ""
+            DBConnection.initialise("jdbc:h2:~/db;MODE=postgresql;INIT=RUNSCRIPT FROM 'src/test/kotlin/resources/createAndPopulateH2Db.sql'")
+            val expected = "[JsonConfigTemplate(fieldName=Product Id, type=text, isNullAllowed=null, pattern=null, length=0, minLength=0, maxLength=0, dependencies=null, values=null), JsonConfigTemplate(fieldName=Product Description, type=email, isNullAllowed=null, pattern=null, length=0, minLength=0, maxLength=0, dependencies=null, values=null), JsonConfigTemplate(fieldName=Price, type=alphabets, isNullAllowed=null, pattern=null, length=0, minLength=0, maxLength=0, dependencies=null, values=null), JsonConfigTemplate(fieldName=Export, type=number, isNullAllowed=null, pattern=null, length=0, minLength=0, maxLength=0, dependencies=null, values=null), JsonConfigTemplate(fieldName=Country Name, type=text, isNullAllowed=null, pattern=null, length=0, minLength=0, maxLength=0, dependencies=null, values=null), JsonConfigTemplate(fieldName=Source City, type=text, isNullAllowed=null, pattern=null, length=0, minLength=0, maxLength=0, dependencies=null, values=null), JsonConfigTemplate(fieldName=Country Code, type=text, isNullAllowed=null, pattern=null, length=0, minLength=0, maxLength=0, dependencies=null, values=null), JsonConfigTemplate(fieldName=Source Pincode, type=text, isNullAllowed=null, pattern=null, length=0, minLength=0, maxLength=0, dependencies=null, values=null)]"
 
             ConfigReaderWriter().writeConfig(configName, jsonData)
-            val actual = "executeQuery()"
+            val actual = ConfigReaderWriter().readConfig(configName).toString()
 
             assertEquals(expected, actual)
         }
 
         @Test
         fun shouldWriteTheDataWithLengthParameters() {
-            val configName = "nameAndType"
+            val configName = "nameAndTypeWithLength"
             val jsonData = createArrayOfJsonTemplate( length = true)
-            val expected = ""
+            val expected = "[JsonConfigTemplate(fieldName=Product Id, type=text, isNullAllowed=null, pattern=null, length=0, minLength=2, maxLength=5, dependencies=null, values=null), JsonConfigTemplate(fieldName=Product Description, type=email, isNullAllowed=null, pattern=null, length=7, minLength=0, maxLength=0, dependencies=null, values=null), JsonConfigTemplate(fieldName=Price, type=alphabets, isNullAllowed=null, pattern=null, length=0, minLength=8, maxLength=12, dependencies=null, values=null), JsonConfigTemplate(fieldName=Export, type=number, isNullAllowed=null, pattern=null, length=115, minLength=0, maxLength=0, dependencies=null, values=null), JsonConfigTemplate(fieldName=Country Name, type=text, isNullAllowed=null, pattern=null, length=0, minLength=0, maxLength=5, dependencies=null, values=null), JsonConfigTemplate(fieldName=Source City, type=text, isNullAllowed=null, pattern=null, length=0, minLength=3, maxLength=0, dependencies=null, values=null), JsonConfigTemplate(fieldName=Country Code, type=text, isNullAllowed=null, pattern=null, length=0, minLength=0, maxLength=3, dependencies=null, values=null), JsonConfigTemplate(fieldName=Source Pincode, type=text, isNullAllowed=null, pattern=null, length=6, minLength=0, maxLength=0, dependencies=null, values=null)]"
+            DBConnection.initialise("jdbc:h2:~/db;MODE=postgresql;INIT=RUNSCRIPT FROM 'src/test/kotlin/resources/createAndPopulateH2Db.sql'")
 
             ConfigReaderWriter().writeConfig(configName, jsonData)
-            val actual = "executeQuery()"
+            val actual = ConfigReaderWriter().readConfig(configName).toString()
 
             assertEquals(expected, actual)
         }
 
         @Test
         fun shouldWriteTheDataWithDependencies() {
-            val configName = "nameAndType"
+            val configName = "nameAndTypeWithDependencies"
             val jsonData = createArrayOfJsonTemplate(dependency = true)
-            val expected = ""
+            val expected = "[JsonConfigTemplate(fieldName=Product Id, type=text, isNullAllowed=null, pattern=null, length=0, minLength=0, maxLength=0, dependencies=[DependencyTemplate(dependentOn=Price, expectedDependentFieldValue=null, expectedCurrentFieldValue=null)], values=null), JsonConfigTemplate(fieldName=Product Description, type=email, isNullAllowed=null, pattern=null, length=0, minLength=0, maxLength=0, dependencies=[DependencyTemplate(dependentOn=Export, expectedDependentFieldValue=!null, expectedCurrentFieldValue=!null)], values=null), JsonConfigTemplate(fieldName=Price, type=alphabets, isNullAllowed=null, pattern=null, length=0, minLength=0, maxLength=0, dependencies=[DependencyTemplate(dependentOn=Export, expectedDependentFieldValue=!null, expectedCurrentFieldValue=null)], values=null)]"
+            DBConnection.initialise("jdbc:h2:~/db;MODE=postgresql;INIT=RUNSCRIPT FROM 'src/test/kotlin/resources/createAndPopulateH2Db.sql'")
 
             ConfigReaderWriter().writeConfig(configName, jsonData)
-            val actual = "executeQuery()"
+            val actual = ConfigReaderWriter().readConfig(configName).toString()
 
             assertEquals(expected, actual)
         }
 
         @Test
         fun shouldWriteTheDataWithDateTimePattern() {
-            val configName = "nameAndType"
+            val configName = "nameAndTypeAsDateTime"
             val jsonData = createArrayOfJsonTemplate(dateTime = true)
-            val expected = ""
+            val expected = "[JsonConfigTemplate(fieldName=Product Id, type=date-time, isNullAllowed=null, pattern=hh:ss:mm a,uuuu-MM-dd, length=0, minLength=0, maxLength=0, dependencies=null, values=null), JsonConfigTemplate(fieldName=Product Description, type=time, isNullAllowed=null, pattern=hh:ss:mm a, length=0, minLength=0, maxLength=0, dependencies=null, values=null), JsonConfigTemplate(fieldName=Price, type=date, isNullAllowed=null, pattern=dd-MM-uuuu, length=0, minLength=0, maxLength=0, dependencies=null, values=null)]"
+            DBConnection.initialise("jdbc:h2:~/db;MODE=postgresql;INIT=RUNSCRIPT FROM 'src/test/kotlin/resources/createAndPopulateH2Db.sql'")
 
             ConfigReaderWriter().writeConfig(configName, jsonData)
-            val actual = "executeQuery()"
+            val actual = ConfigReaderWriter().readConfig(configName).toString()
 
             assertEquals(expected, actual)
         }
@@ -86,13 +90,13 @@ internal class ConfigReaderWriterTest {
         ): Array<JsonConfigTemplate> {
 
             if (length) {
-                generateArrayOfConfigWithLengthParameters()
+                return generateArrayOfConfigWithLengthParameters()
             }
             if (dependency) {
-                generateArrayOfConfigWithDependencyParameters()
+                return generateArrayOfConfigWithDependencyParameters()
             }
             if (dateTime) {
-                generateArrayOfConfigWithDateAndTimeParameters()
+                return generateArrayOfConfigWithDateAndTimeParameters()
             }
             return generateArrayOfConfigWithoutAnyExtraParameters()
         }
