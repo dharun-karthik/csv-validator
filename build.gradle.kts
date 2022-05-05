8a9be2d870c5d72760a74872c93f70c815b0e37c
DharunKarthik <dharunk@sahaj.ai>
2022-05-05 10:03:59 +0530

    Added h2db dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
     implementation("com.google.code.gson:gson:2.9.0")
     implementation("org.postgresql:postgresql:42.3.4")
+    testImplementation("com.h2database:h2:1.3.148")
 
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.8.2")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
