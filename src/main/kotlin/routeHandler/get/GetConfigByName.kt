339d9a2e5af71528de9044e8fb4a6f6411797ad1
Shobhit Aryan <shobhitl@sahaj.ai>
2022-05-06 13:10:03 +0530

    Fixed bug: Sending response for configuration from database in right json format

--- src/main/kotlin/routeHandler/get/GetConfigByName.kt
+++ src/main/kotlin/routeHandler/get/GetConfigByName.kt
@@ -1,6 +1,7 @@
 package routeHandler.get
 
 import db.DBConfigReaderWriter
+import org.json.JSONArray
 import request.RequestHandler
 import response.ContentType
 import response.Response
@@ -11,8 +12,10 @@ class GetConfigByName {
         val response = Response()
         val configName = requestHandler.getHeaderFieldValue(request, "config-name")
             ?: return response.generateResponse("No config name", 400, ContentType.HTML.value)
-        val body = DBConfigReaderWriter().readConfig(configName).toString()
-        return response.generateResponse(body, 200, ContentType.JSON.value)
+        val body = DBConfigReaderWriter().readConfig(configName)
+        val bodyInJsonFormat = JSONArray(body)
+        val bodyInStringFormat = bodyInJsonFormat.toString()
+        return response.generateResponse(bodyInStringFormat, 200, ContentType.JSON.value)
     }
 
 }
---
+++ src/test/kotlin/routeHandler/get/GetConfigByNameTest.kt
@@ -0,0 +1,5 @@
+package routeHandler.get
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class GetConfigByNameTest
