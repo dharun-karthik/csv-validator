09d02b48da5d7963010573a7f3f84ea0fe6455ac
Shobhit Aryan <shobhitl@sahaj.ai>
2022-05-06 16:40:46 +0530

    Added test to get rule from saved rules in db

--- src/test/kotlin/routeHandler/get/GetConfigByNameTest.kt
+++ src/test/kotlin/routeHandler/get/GetConfigByNameTest.kt
@@ -1,5 +1,38 @@
 package routeHandler.get
 
-import org.junit.jupiter.api.Assertions.*
+import db.DBConnection
+import fakeStreams.FakeInputStreamProvider
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import routeHandler.post.ConfigAdder
 
-internal class GetConfigByNameTest
+internal class GetConfigByNameTest {
+
+    @Test
+    fun shouldGetConfigForRuleNameConfig_1() {
+        addDataToH2Database()
+        val getConfigByName = GetConfigByName()
+        val configuration_name = "config_1"
+        val request = """GET /get-config HTTP/1.1
+                        |config-name: $configuration_name
+                      """.trimMargin()
+        val actual = getConfigByName.handle(request)
+        val expected = """HTTP/1.1 200 OK
+                         |Content-Type: application/json; charset=utf-8
+                         |Content-Length: 171
+
+                         |[{"fieldName":"Product Id","minLength":0,"length":0,"type":"text","maxLength":0},{"fieldName":"Product Description","minLength":0,"length":0,"type":"email","maxLength":0}]""".trimMargin()
+        assertEquals(expected, actual)
+    }
+
+    private fun addDataToH2Database() {
+        val jsonString = """{"config_1":[{"fieldName":"Product Id","type":"text"},{"fieldName":"Product Description","type":"email"}]}"""
+        val inputStream = FakeInputStreamProvider(jsonString)
+        val request = """
+            Content-Length: ${jsonString.length}
+            
+        """
+        DBConnection.initialise("jdbc:h2:~/db;MODE=postgresql;INIT=RUNSCRIPT FROM 'src/test/kotlin/resources/createAndPopulateH2Db.sql'")
+        ConfigAdder().handle(request, inputStream)
+    }
+}
