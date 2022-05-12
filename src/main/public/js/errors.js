cfe1e44644189fb04c2df76dbf7f69510c6e4bd7
Shobhit Aryan <shobhitl@sahaj.ai>
2022-05-12 18:05:51 +0530

    Commented previous logic of extracting error details from json

--- src/main/public/js/errors.js
+++ src/main/public/js/errors.js
@@ -45,64 +45,64 @@ function printCsvValid() {
 }
 
 
-async function displayErrors() {
-    clearPreviousErrors()
-    for (key in errors) {
-        let errorListContainer = document.createElement('div')
-        errorListContainer.className = 'error-list'
+// async function displayErrors() {
+//     clearPreviousErrors()
+//     for (key in errors) {
+//         let errorListContainer = document.createElement('div')
+//         errorListContainer.className = 'error-list'
 
-        displayErrorsForAllLines(errorListContainer);
-        document.getElementById('display-errors').appendChild(errorListContainer)
-    }
-}
+//         displayErrorsForAllLines(errorListContainer);
+//         document.getElementById('display-errors').appendChild(errorListContainer)
+//     }
+// }
 
-function displayErrorsForAllLines(errorListContainer) {
-    for (lineNumber in errors[key]) {
-        errorsInLineNumber = errors[key][lineNumber];
-        if (lineNumber == '0') {
-            displayColumnErrors(errorsInLineNumber)
-            return
-        }
-        let lineNo = document.createElement('div');
-        lineNo.className = 'line-number';
-        lineNo.innerHTML = `Line No:
-            <span>${lineNumber}</span>`;
+// function displayErrorsForAllLines(errorListContainer) {
+//     for (lineNumber in errors[key]) {
+//         errorsInLineNumber = errors[key][lineNumber];
+//         if (lineNumber == '0') {
+//             displayColumnErrors(errorsInLineNumber)
+//             return
+//         }
+//         let lineNo = document.createElement('div');
+//         lineNo.className = 'line-number';
+//         lineNo.innerHTML = `Line No:
+//             <span>${lineNumber}</span>`;
 
-        errorListContainer.appendChild(lineNo);
+//         errorListContainer.appendChild(lineNo);
 
-        let allErrorsInOneLine = document.createElement('div');
-        allErrorsInOneLine.className = 'errors';
+//         let allErrorsInOneLine = document.createElement('div');
+//         allErrorsInOneLine.className = 'errors';
 
-        displayOneTypeOfError(allErrorsInOneLine);
-        errorListContainer.appendChild(allErrorsInOneLine);
-    }
-}
+//         displayOneTypeOfError(allErrorsInOneLine);
+//         errorListContainer.appendChild(allErrorsInOneLine);
+//     }
+// }
 
-function displayOneTypeOfError(allErrorsInOneLine) {
-    for (errorIndex in errorsInLineNumber) {
-        let errorField = errorsInLineNumber[errorIndex];
-        let errorDiv = document.createElement('div');
-        errorDiv.className = 'error';
+// function displayOneTypeOfError(allErrorsInOneLine) {
+//     for (errorIndex in errorsInLineNumber) {
+//         let errorField = errorsInLineNumber[errorIndex];
+//         let errorDiv = document.createElement('div');
+//         errorDiv.className = 'error';
 
-        errorDiv.innerText = `${errorField}`;
+//         errorDiv.innerText = `${errorField}`;
 
-        allErrorsInOneLine.appendChild(errorDiv);
-    }
-}
+//         allErrorsInOneLine.appendChild(errorDiv);
+//     }
+// }
 
-function displayColumnErrors(incorrectColumns) {
-    let columnNameContainer = document.createElement('div')
-    columnNameContainer.className = 'all-column-errors'
-    incorrectColumns.forEach(incorrectColumn => {
-        let oneColumnErrorDiv = document.createElement('div')
-        oneColumnErrorDiv.className = 'column-error'
-        oneColumnErrorDiv.innerText = `Column ${incorrectColumn} not found in CSV`;
+// function displayColumnErrors(incorrectColumns) {
+//     let columnNameContainer = document.createElement('div')
+//     columnNameContainer.className = 'all-column-errors'
+//     incorrectColumns.forEach(incorrectColumn => {
+//         let oneColumnErrorDiv = document.createElement('div')
+//         oneColumnErrorDiv.className = 'column-error'
+//         oneColumnErrorDiv.innerText = `Column ${incorrectColumn} not found in CSV`;
 
-        columnNameContainer.appendChild(oneColumnErrorDiv)
-    })
-    document.getElementById('display-errors').appendChild(columnNameContainer)
-}
+//         columnNameContainer.appendChild(oneColumnErrorDiv)
+//     })
+//     document.getElementById('display-errors').appendChild(columnNameContainer)
+// }
 
-function clearPreviousErrors() {
-    document.getElementById('display-errors').innerText = "";
-}
+// function clearPreviousErrors() {
+//     document.getElementById('display-errors').innerText = "";
+// }
