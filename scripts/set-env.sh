cc5ce8610eab0e7c332115753db393080f5c9a4e
pratheepmm <pratheepm@sahaj.ai>
2022-05-12 14:54:10 +0530

    added dbname

--- scripts/set-env.sh
+++ scripts/set-env.sh
@@ -2,3 +2,4 @@
 export DB_USERNAME="db_user"
 export DB_PASSWORD="database-user"
 export POSTGRES_HOST_AUTH_METHOD="trust"
+export DB_NAME="postgresql"
--- scripts/set-test-env.sh
+++ scripts/set-test-env.sh
@@ -2,3 +2,4 @@
 export DB_USERNAME="test_user"
 export DB_PASSWORD="test-db-user"
 export POSTGRES_HOST_AUTH_METHOD="trust"
+export DB_NAME="postgresql"
