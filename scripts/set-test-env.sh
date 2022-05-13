77954e61062400c0ba13cf9558bd8139b9392a10
pratheepmm <pratheepm@sahaj.ai>
2022-05-13 10:48:13 +0530

    changed env variables to use env variables provided

--- scripts/set-test-env.sh
+++ scripts/set-test-env.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-export DB_USERNAME="test_user"
-export DB_PASSWORD="test-db-user"
-export POSTGRES_HOST_AUTH_METHOD="trust"
+export DB_USERNAME=$POSTGRES_USER
+export DB_PASSWORD=$POSTGRES_PASSWORD
 export DB_URL="//postgres:5432"
 export DB="postgresql"
-export DB_NAME="csv_validator"
+export DB_NAME=$POSTGRES_DB
