715ac1c11d9644eaa1ce02685f52b2c0c9caafdd
Shobhit Aryan <shobhitl@sahaj.ai>
2022-05-16 14:37:17 +0530

    Fixed bug: Displays summary of all type of errors

--- src/main/public/js/errors.js
+++ src/main/public/js/errors.js
@@ -145,7 +145,10 @@ function displayColumnDetailsOf(index) {
     document.getElementById('details-container').innerHTML = errorTypeHTML
 }
 
-function extractErrorFromWholeErrorLine(line) {
+function extractErrorFromWholeErrorLine(line, shortErrorDescription) {
+    if(!line.match(/(in)/)){
+        return shortErrorDescription   
+    }    
     let indexOfIn = line.match(/(in)/).index - 1
     return line.slice(0, indexOfIn)
 }
@@ -157,7 +160,7 @@ function getErrorTypeHTMLAt(index) {
         <div class="one-error-type-section">
         <div class="error-details-without-lines">
             <div class="error-type-title">
-                <span id="error-type-message"> ${extractErrorFromWholeErrorLine(errorLines[index][indexToFill][0][1])} </span> in <span id="error-count"
+                <span id="error-type-message"> ${extractErrorFromWholeErrorLine(errorLines[index][indexToFill][0][1], errorTypes[index][indexToFill][0])} </span> in <span id="error-count"
                     class="column-name"> ${errorTypes[index][indexToFill][1]} </span> lines
             </div>
             <button id="error${indexToFill}" class="button no-margin" onclick="displayLinesForErrorType(this)">See <span>More</span></button>
